65b3a4bcb47fb4391bd556e1a65a37e99cd500be
Stepasha419a <Dark_Magic419a@mail.ru>
2022-08-11 11:27:58 +0300

    working check mark icon new checked logic

--- client/src/components/Profile/ProfileSetting.tsx
+++ client/src/components/Profile/ProfileSetting.tsx
@@ -1,3 +1,5 @@
+import { faCheck } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useEffect, useState } from "react"
 import { IUser } from "../../models/IUser"
 
@@ -29,7 +31,31 @@ const ProfileSetting: React.FC<ProfileSettingPropsInterface> = ({currentUser, se
                 {formName}
             </div>
             <div className="tinder__content-setting-change">
-                <input onChange={(e) => setInputValue(e.target.value)} value={inputValue} type="text" className="tinder__content-setting-change-input"/>
+                {settingInputName === 'preferSex' && innerObjectName === 'partnerSettings' ?
+
+                    <div>
+                        <div onClick={() => setInputValue('male')} className={"tinder__content-setting-change-input-wrapper " + (inputValue === 'male' ? "tinder__content-setting-change-input-wrapper--active" : "") }>
+                            <input name={settingInputName} type="radio" value="male" id={settingInputName + '1'} checked={inputValue === 'male'} className="tinder__content-setting-change-input-radio"/>
+                            <label htmlFor={settingInputName + '1'}>
+                                Male
+                            </label>
+                            {inputValue === 'male' &&
+                            <FontAwesomeIcon icon={faCheck} className="tinder__content-setting-change-checked"/>}
+                        </div>
+                        <div onClick={() => setInputValue('female')} className={"tinder__content-setting-change-input-wrapper " + (inputValue === 'female' ? "tinder__content-setting-change-input-wrapper--active" : "") }>
+                            <input name={settingInputName} type="radio" value="female"  id={settingInputName + '2'} checked={inputValue === 'female'} className="tinder__content-setting-change-input-radio"/>
+                            <label htmlFor={settingInputName + '2'}>
+                                Female
+                            </label>
+                            {inputValue === 'female' &&
+                            <FontAwesomeIcon icon={faCheck} className="tinder__content-setting-change-checked"/>}
+                        </div>
+                    </div>
+
+                :
+
+                    <input onChange={(e) => setInputValue(e.target.value)} value={inputValue} type="text" className="tinder__content-setting-change-input"/>
+                }
             </div>
             <div className="tinder__content-setting-descr">Your {formName}</div>
             <button onClick={() => cancelHandler()} className="tinder__content-setting-submit-button tinder__content-setting-submit-button--margin-bottom">
