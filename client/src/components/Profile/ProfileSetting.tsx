0af43ffdfe9350468322d64f0782da4765a968a6
Stepasha419a <Dark_Magic419a@mail.ru>
2022-08-21 03:02:20 +0300

    added profile settings form validation

--- client/src/components/Profile/ProfileSetting.tsx
+++ client/src/components/Profile/ProfileSetting.tsx
@@ -1,6 +1,6 @@
 import { faCheck } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { useEffect, useState } from "react"
+import { ChangeEvent, useEffect, useState } from "react"
 import { IUser } from "../../models/IUser"
 
 interface ProfileSettingPropsInterface {
@@ -12,9 +12,47 @@ interface ProfileSettingPropsInterface {
     innerObjectName: string
     setInnerObjectName: (innerObjectName: string) => void
 }
-// When u call innerObjectName input so currentUser[settingInputName] after that is not available, maybe because of useState startment argument
+
 const ProfileSetting: React.FC<ProfileSettingPropsInterface> = ({currentUser, setIsUserInfoSetting, submitSettings, formName, settingInputName, innerObjectName, setInnerObjectName}) => {
     const [inputValue, setInputValue] = useState('')
+    const [inputValueDirty, setInputValueDirty] = useState(false)
+    const [inputValueError, setInputValueError] = useState('')
+    const [isFormValid, setIsFormValid] = useState(false)
+    const [isFormCloseable, setIsFormCloseable] = useState(true)
+
+    const inputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value)
+        if(e.target.value.length < 2 || e.target.value.length > 15) {
+            setInputValueError(`${formName} has to be more 2 and less 15`)
+          if(!e.target.value) {
+            setInputValueError(`${formName} can't be empty`)
+          }
+        } else{
+            setInputValueError('')
+        }
+    }
+
+    useEffect(() => {
+        if(inputValueError) {
+          setIsFormValid(false)
+        } else {
+          setIsFormValid(true)
+        }
+    }, [inputValueError])
+
+    useEffect(() => {
+        if((settingInputName === 'preferSex' && innerObjectName === 'partnerSettings') || (settingInputName === 'sex' && innerObjectName === '')) {
+            if(!inputValue) {
+                setIsFormValid(false)
+                setIsFormCloseable(false)
+                setInputValueError('Form can\'t be empty')
+            } else {
+                setIsFormValid(true)
+                setIsFormCloseable(true)
+                setInputValueError('')
+            }
+        }
+    }, [settingInputName, innerObjectName, inputValue])
 
     useEffect(() => { //@ts-ignore
         setInputValue(innerObjectName ? currentUser[innerObjectName][settingInputName] : currentUser[settingInputName])
@@ -27,6 +65,11 @@ const ProfileSetting: React.FC<ProfileSettingPropsInterface> = ({currentUser, se
     
     return (
         <div className="tinder__content-setting">
+            {inputValueDirty &&
+                <div className="tinder__content-setting-name tinder__content-setting-name--error">
+                    {inputValueError}
+                </div>
+            }
             <div className="tinder__content-setting-name">
                 {formName}
             </div>
@@ -54,14 +97,20 @@ const ProfileSetting: React.FC<ProfileSettingPropsInterface> = ({currentUser, se
 
                 :
 
-                    <input onChange={(e) => setInputValue(e.target.value)} value={inputValue} type="text" className="tinder__content-setting-change-input"/>
+                    <input 
+                        onChange={(e) => inputHandler(e)} 
+                        onBlur={(() => setInputValueDirty(true))} 
+                        value={inputValue} 
+                        type="text" 
+                        className="tinder__content-setting-change-input"
+                    />
                 }
             </div>
             <div className="tinder__content-setting-descr">Your {formName}</div>
-            <button onClick={() => cancelHandler()} className="tinder__content-setting-submit-button tinder__content-setting-submit-button--margin-bottom">
+            <button disabled={!isFormCloseable} onClick={() => cancelHandler()} className="tinder__content-setting-submit-button tinder__content-setting-submit-button--margin-bottom">
                 Cancel
             </button>
-            <button onClick={() => submitSettings(settingInputName, inputValue, innerObjectName)} className="tinder__content-setting-submit-button">
+            <button disabled={!isFormValid} onClick={() => submitSettings(settingInputName, inputValue, innerObjectName)} className="tinder__content-setting-submit-button">
                 Update my {formName}
             </button>
         </div>
