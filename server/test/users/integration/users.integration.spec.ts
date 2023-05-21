7ceaf70113bf375037ab72e43a1170e9edf347ad
Stepasha419a <Dark_Magic419a@mail.ru>
2023-05-21 23:23:40 +0300

    test: sorted, picture (post, put), picture/mix, pairs (post) users integration tests implementation

--- server/test/users/integration/users.integration.spec.ts
+++ server/test/users/integration/users.integration.spec.ts
@@ -6,15 +6,22 @@ import { AppModule } from 'app.module';
 import { AuthGuard } from 'auth/auth.guard';
 import prismaClient from '../../prisma-client/prisma-client';
 import { newUserStub } from '../stubs';
-import { User } from '@prisma/client';
-import { UPDATE_USER_DTO } from '../values/users-const.dto';
+import {
+  UPDATE_USER_DTO,
+  USER_SORTS_DTO,
+  DELETE_PICTURE_DTO,
+  MIX_PICTURES_DTO,
+} from '../values/users-const.dto';
+import * as path from 'path';
+import { UserDto } from 'users/dto';
+import { UsersSelector } from 'users/utils';
 
 describe('UsersController', () => {
   let httpServer: HttpServer;
   let app: NestApplication;
 
-  let users: User[];
-  let currentUser: User;
+  let users: UserDto[];
+  let currentUser: UserDto;
 
   const mockAuthGuard = jest.fn().mockReturnValue(true);
 
@@ -33,23 +40,77 @@ describe('UsersController', () => {
     await prismaClient.user.createMany({
       data: [
         { email: '123@gmail.com', password: '123123', name: 'Jason' },
-        { email: '456@gmail.com', password: '456456', name: 'Loren' },
+        {
+          email: '456@gmail.com',
+          password: '456456',
+          name: 'Loren',
+          age: 20,
+          distance: 50,
+          preferAgeFrom: 18,
+          preferAgeTo: 28,
+          preferSex: 'male',
+          sex: 'female',
+        },
       ],
     });
+
+    users = (
+      await prismaClient.user.findMany({
+        include: UsersSelector.selectUser(),
+      })
+    ).map((user) => new UserDto(user));
+
+    users[1] = new UserDto(
+      await prismaClient.user.update({
+        where: { id: users[1].id },
+        data: {
+          pictures: {
+            createMany: {
+              data: [
+                { name: `picture-${users[1].id}`, order: 0 },
+                { name: `picture-${users[1].id}`, order: 1 },
+                { name: `picture-${users[1].id}`, order: 2 },
+                { name: `picture-${users[1].id}`, order: 3 },
+                { name: `picture-${users[1].id}`, order: 4 },
+                { name: `picture-${users[1].id}`, order: 5 },
+                { name: `picture-${users[1].id}`, order: 6 },
+                { name: `picture-${users[1].id}`, order: 7 },
+                { name: `picture-${users[1].id}`, order: 8 },
+              ],
+            },
+          },
+        },
+        include: UsersSelector.selectUser(),
+      }),
+    );
 
     await prismaClient.interest.createMany({
       data: [{ name: 'traveling' }, { name: 'ski' }],
     });
 
-    users = await prismaClient.user.findMany();
-    currentUser = users[0];
+    currentUser = new UserDto(
+      await prismaClient.user.findUnique({
+        where: { id: users[0].id },
+        include: UsersSelector.selectUser(),
+      }),
+    );
 
     httpServer = app.getHttpServer();
+  });
+
+  afterEach(async () => {
+    currentUser = new UserDto(
+      await prismaClient.user.findUnique({
+        where: { id: users[0].id },
+        include: UsersSelector.selectUser(),
+      }),
+    );
   });
 
   afterAll(async () => {
     await app.close();
     await prismaClient.$transaction([
+      prismaClient.picture.deleteMany(),
       prismaClient.user.deleteMany(),
       prismaClient.interest.deleteMany(),
     ]);
@@ -93,7 +154,7 @@ describe('UsersController', () => {
 
       beforeAll(async () => {
         response = await request(httpServer)
-          .patch(`/users/wrong-id`)
+          .patch('/users/wrong-id')
           .send({ name: 'William' });
       });
 
@@ -104,82 +165,293 @@ describe('UsersController', () => {
     });
   });
 
-  /* describe('users/sorted (POST)', () => {
-    const user = userStub();
-    const userSortsDto: UserSortsDto = {
-      distance: user.distance + 10,
-      onlyNear: false,
-      age: user.age,
-      preferAgeFrom: user.age - 2,
-      preferAgeTo: user.age + 2,
-      sex: 'female',
-      preferSex: 'male',
-      userIds: [],
-    };
+  describe('users/sorted (POST)', () => {
+    describe('when it is called correctly', () => {
+      let response: request.Response;
 
-    it('should return a sorted user', async () => {
-      const response = await request(httpServer)
-        .post('/users/sorted')
-        .send(userSortsDto);
+      beforeAll(async () => {
+        response = await request(httpServer)
+          .post('/users/sorted')
+          .send(USER_SORTS_DTO);
+      });
 
-      expect(response.status).toBe(200);
-      expect(response.body).toEqual({ ...user, id: userId });
+      it('should return a user', async () => {
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({
+          id: users[1].id,
+          age: users[1].age,
+          description: users[1].description,
+          distance: users[1].distance,
+          name: users[1].name,
+          pictures: users[1].pictures,
+          interests: [],
+        });
+      });
     });
-  }); */
 
-  /* describe('users/picture (POST)', () => {
-    const user = userStub();
-    const savePictureDto: SavePictureDto = {
-      userId,
-    };
+    describe('when there is no such user', () => {
+      let response: request.Response;
 
-    it('should return an updated user with updated pictures field', async () => {
-      const result = await dbConnection
-        .collection('users')
-        .findOne({ email: userStub().email });
-      userId = result.id.toString();
+      beforeAll(async () => {
+        response = await request(httpServer)
+          .post('/users/sorted')
+          .send({ ...USER_SORTS_DTO, userIds: [users[0].id, users[1].id] });
+      });
 
-      const response = await request(httpServer)
-        .post('/users/picture')
-        .field('userId', savePictureDto.userId)
-        .attach(
-          'picture',
-          path.resolve(__dirname, '..', 'stubs', 'test-image.jpg'),
+      it('should throw an error', async () => {
+        expect(response.status).toBe(404);
+        expect(response.body.message).toEqual(
+          'Such user was not found, try to change settings',
         );
-
-      expect(response.status).toBe(201);
-      expect(response.body).toEqual({
-        ...user,
-        id: userId.toString(),
-        pictures: { ...user.pictures, avatar: response.body.pictures.avatar },
       });
     });
-  }); */
+  });
 
-  /* describe('users/picture (PUT)', () => {
-    const user = userStub();
+  describe('users/picture (POST)', () => {
+    describe('when it is called correctly', () => {
+      let response: request.Response;
 
-    it('should return an updated user with updated pictures field', async () => {
-      const result = await dbConnection
-        .collection('users')
-        .findOne({ email: userStub().email });
-      userId = result.id.toString();
+      beforeAll(async () => {
+        response = await request(httpServer)
+          .post('/users/picture')
+          .field('userId', currentUser.id)
+          .attach(
+            'picture',
+            path.resolve(__dirname, '..', 'stubs', 'test-image.jpg'),
+          );
+      });
 
-      const deletePictureDto: DeletePictureDto = {
-        userId,
-        order: 0,
-      };
-
-      const response = await request(httpServer)
-        .put('/users/picture')
-        .send(deletePictureDto);
-
-      expect(response.status).toBe(200);
-      expect(response.body).toEqual({
-        ...user,
-        id: userId.toString(),
-        pictures: { ...user.pictures, avatar: '' },
+      it('should return a user', async () => {
+        expect(response.status).toBe(201);
+        expect(response.body).toEqual(
+          new UserDto({
+            ...currentUser,
+            _count: { pairFor: 0 },
+            pictures: [
+              {
+                name: response.body.pictures[0].name,
+                order: 0,
+              },
+            ],
+          }),
+        );
       });
     });
-  }); */
+
+    describe('when there is no such user', () => {
+      let response: request.Response;
+
+      beforeAll(async () => {
+        response = await request(httpServer)
+          .post('/users/picture')
+          .field('userId', 'wrong-id')
+          .attach(
+            'picture',
+            path.resolve(__dirname, '..', 'stubs', 'test-image.jpg'),
+          );
+      });
+
+      it('should throw an error', async () => {
+        expect(response.status).toBe(404);
+        expect(response.body.message).toEqual('Such user was not found');
+      });
+    });
+
+    describe('when it is max pictures count', () => {
+      let response: request.Response;
+
+      beforeAll(async () => {
+        response = await request(httpServer)
+          .post('/users/picture')
+          .field('userId', users[1].id)
+          .attach(
+            'picture',
+            path.resolve(__dirname, '..', 'stubs', 'test-image.jpg'),
+          );
+      });
+
+      it('should throw an error', async () => {
+        expect(response.status).toBe(400);
+        expect(response.body.message).toEqual('You have max pictures count');
+      });
+    });
+  });
+
+  describe('users/picture (PUT)', () => {
+    describe('when it is called correctly', () => {
+      let response: request.Response;
+
+      beforeAll(async () => {
+        response = await request(httpServer)
+          .put('/users/picture')
+          .send({ ...DELETE_PICTURE_DTO, userId: currentUser.id });
+      });
+
+      it('should return a user', async () => {
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({
+          ...currentUser,
+          pictures: [],
+        });
+      });
+    });
+
+    describe('when there is no such picture', () => {
+      let response: request.Response;
+
+      beforeAll(async () => {
+        response = await request(httpServer)
+          .put('/users/picture')
+          .send({ order: 1, userId: currentUser.id });
+      });
+
+      it('should throw an error', async () => {
+        expect(response.status).toBe(404);
+        expect(response.body.message).toEqual('Not Found');
+      });
+    });
+  });
+
+  describe('users/picture/mix (PUT)', () => {
+    describe('when it is called correctly', () => {
+      let response: request.Response;
+
+      beforeAll(async () => {
+        response = await request(httpServer)
+          .put('/users/picture/mix')
+          .send({ ...MIX_PICTURES_DTO, userId: users[1].id });
+      });
+
+      it('should return a user', async () => {
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({
+          ...users[1],
+          pictures: [
+            { ...users[1].pictures[1], order: 0 },
+            { ...users[1].pictures[0], order: 1 },
+            ...users[1].pictures.slice(2),
+          ],
+        });
+      });
+    });
+
+    describe('when there is no such user', () => {
+      let response: request.Response;
+
+      beforeAll(async () => {
+        response = await request(httpServer)
+          .put('/users/picture/mix')
+          .send({ ...MIX_PICTURES_DTO, userId: 'wrong-id' });
+      });
+
+      it('should throw an error', async () => {
+        expect(response.status).toBe(404);
+        expect(response.body.message).toEqual('Such user was not found');
+      });
+    });
+
+    describe('when there is no such picture', () => {
+      let response: request.Response;
+
+      beforeAll(async () => {
+        response = await request(httpServer)
+          .put('/users/picture/mix')
+          .send({ mixOrder: 10, withOrder: 1, userId: users[1].id });
+      });
+
+      it('should throw an error', async () => {
+        expect(response.status).toBe(404);
+        expect(response.body.message).toEqual('Not Found');
+      });
+    });
+
+    describe('when there is no such picture', () => {
+      let response: request.Response;
+
+      beforeAll(async () => {
+        response = await request(httpServer)
+          .put('/users/picture/mix')
+          .send({ mixOrder: 1, withOrder: 10, userId: users[1].id });
+      });
+
+      it('should throw an error', async () => {
+        expect(response.status).toBe(404);
+        expect(response.body.message).toEqual('Not Found');
+      });
+    });
+  });
+
+  describe('users/pairs (POST)', () => {
+    describe('when it is called correctly', () => {
+      let response: request.Response;
+
+      beforeAll(async () => {
+        response = await request(httpServer)
+          .post('/users/pairs')
+          .send({ userId: currentUser.id, userPairId: users[1].id });
+      });
+
+      it('should return a user', async () => {
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual([
+          {
+            id: users[1].id,
+            name: users[1].name,
+            description: users[1].description,
+            distance: users[1].distance,
+            interests: users[1].interests,
+            age: users[1].age,
+            pictures: users[1].pictures,
+          },
+        ]);
+      });
+    });
+
+    describe('when there is no such user', () => {
+      let response: request.Response;
+
+      beforeAll(async () => {
+        response = await request(httpServer)
+          .post('/users/pairs')
+          .send({ userId: 'wrong-id', userPairId: users[1].id });
+      });
+
+      it('should throw an error', async () => {
+        expect(response.status).toBe(404);
+        expect(response.body.message).toEqual('Such user was not found');
+      });
+    });
+
+    describe('when there is no such user', () => {
+      let response: request.Response;
+
+      beforeAll(async () => {
+        response = await request(httpServer)
+          .post('/users/pairs')
+          .send({ userId: currentUser.id, userPairId: 'wrong-id' });
+      });
+
+      it('should throw an error', async () => {
+        expect(response.status).toBe(404);
+        expect(response.body.message).toEqual('Such user was not found');
+      });
+    });
+
+    describe('when they are already paired => already exist', () => {
+      let response: request.Response;
+
+      beforeAll(async () => {
+        response = await request(httpServer)
+          .post('/users/pairs')
+          .send({ userId: currentUser.id, userPairId: users[1].id });
+      });
+
+      it('should throw an error', async () => {
+        expect(response.status).toBe(404);
+        expect(response.body.message).toEqual(
+          'Pair with such an id already exists',
+        );
+      });
+    });
+  });
 });
