c825140aad141bc341a956faf50a255521025d01
Stepan <Dark_Magic419a@mail.ru>
2023-08-06 23:30:48 +0300

    feature: tinder page, user preview (full version, and some ui) mobile adaptive

--- client/src/features/tinder/RateUser/RateUser.tsx
+++ client/src/features/tinder/RateUser/RateUser.tsx
@@ -1,16 +1,23 @@
 import type { FC } from 'react';
 import { useState, useEffect } from 'react';
 import { motion, useAnimationControls } from 'framer-motion';
-import { useAppDispatch, useAppSelector } from '@shared/lib/hooks';
+import {
+  useAppDispatch,
+  useAppSelector,
+  useMediaQuery,
+} from '@shared/lib/hooks';
 import { Preview } from '@entities/user/components';
 import { getSortedUserThunk, selectTinderData } from '@entities/tinder/model';
 import { RateButtons, Failed } from './components';
 import { RateUserLazy } from './RateUser.lazy';
 import { useSwipeProps } from '../lib';
 import styles from './RateUser.module.scss';
+import classNames from 'classnames';
 
 export const RateUser: FC = () => {
   const dispatch = useAppDispatch();
+
+  const isMobile = useMediaQuery('(max-width: 900px)');
 
   const { tinderUser, isFailed } = useAppSelector(selectTinderData);
   const isLoading = useAppSelector((state) => state.tinder.isLoading);
@@ -30,6 +37,24 @@ export const RateUser: FC = () => {
 
   if (!tinderUser || isLoading) {
     return <RateUserLazy />;
+  }
+
+  if (isMobile) {
+    return (
+      <motion.div {...motionProps}>
+        <Preview
+          user={tinderUser}
+          setIsFullPreview={setIsFullPreview}
+          extraClassName={classNames(
+            isFullPreview ? styles.padding : styles.grabbing,
+            styles.mobile
+          )}
+          isFull={isFullPreview}
+          isShadow={!isFullPreview}
+        />
+        <RateButtons controls={controls} isFullPreview={isFullPreview} />
+      </motion.div>
+    );
   }
 
   return (
--- client/src/pages/Tinder/Tinder.tsx
+++ client/src/pages/Tinder/Tinder.tsx
@@ -2,11 +2,15 @@ import type { FC, ReactElement } from 'react';
 import { RateUser } from '@features/tinder';
 import { Instructions } from './components';
 import styles from './Tinder.module.scss';
+import { useMediaQuery } from '@/shared/lib/hooks';
+import classNames from 'classnames';
 
 const Tinder: FC = (): ReactElement => {
+  const isMobile = useMediaQuery('(max-width: 900px)');
+
   return (
     <>
-      <div className={styles.wrapper}>
+      <div className={classNames(styles.wrapper, isMobile && styles.mobile)}>
         <div className={styles.users}>
           <RateUser />
         </div>
