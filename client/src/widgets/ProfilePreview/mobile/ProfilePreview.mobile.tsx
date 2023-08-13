bfecc29ca95e70a0657fdc52b9016841d705641d
Stepan <Dark_Magic419a@mail.ru>
2023-08-13 21:48:59 +0300

    feature: photos mobile profile preview uploading (from just button)

--- client/src/widgets/ProfilePreview/mobile/ProfilePreview.mobile.tsx
+++ client/src/widgets/ProfilePreview/mobile/ProfilePreview.mobile.tsx
@@ -1,63 +1,84 @@
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
-import { useAppSelector } from '@shared/lib/hooks';
+import { useAppDispatch, useAppSelector } from '@shared/lib/hooks';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCamera, faGear, faPen } from '@fortawesome/free-solid-svg-icons';
 import { Avatar } from '@shared/ui';
 import { ROUTES } from '@shared/constants';
-import { selectPreviewUser } from '@entities/user/model';
+import { selectPreviewUser, setIsDialogUploadOpen } from '@entities/user/model';
 import styles from './ProfilePreview.mobile.module.scss';
+import { CropImage, DialogUpload } from '@/features/user';
 
 export const ProfilePreviewMobile = () => {
+  const dispatch = useAppDispatch();
+
   const previewUser = useAppSelector(selectPreviewUser);
+  const isDialogUploadOpen = useAppSelector(
+    (state) => state.user.profileSetting.isDialogUploadOpen
+  );
+  const isImageCropOpen = useAppSelector(
+    (state) => state.user.profileSetting.isImageCropOpen
+  );
+
+  const openSettingHandler = (): void => {
+    dispatch(setIsDialogUploadOpen(true));
+  };
 
   return (
-    <div className={styles.profile}>
-      <div className={styles.miniature}>
-        <div className={styles.wrapper}>
-          <Link to={`${ROUTES.profile}/edit`} className={styles.user}>
-            <div className={styles.avatarWrapper}>
-              <Avatar
-                userId={previewUser.id}
-                avatarUrl={previewUser.pictures[0]?.name}
-                size="xl"
-                extraClassName={styles.avatar}
-              />
+    <>
+      <div className={styles.profile}>
+        <div className={styles.miniature}>
+          <div className={styles.wrapper}>
+            <Link to={`${ROUTES.profile}/edit`} className={styles.user}>
+              <div className={styles.avatarWrapper}>
+                <Avatar
+                  userId={previewUser.id}
+                  avatarUrl={previewUser.pictures[0]?.name}
+                  size="xl"
+                  extraClassName={styles.avatar}
+                />
+              </div>
+              <div className={styles.descr}>
+                <div className={styles.name}>{previewUser.name}</div>
+                <div className={styles.age}>{previewUser.age}</div>
+              </div>
+            </Link>
+            <div className={styles.actions}>
+              <Link
+                to={ROUTES.settings}
+                className={classNames(styles.action, styles.top)}
+              >
+                <div className={styles.iconWrapper}>
+                  <FontAwesomeIcon className={styles.icon} icon={faGear} />
+                </div>
+                Settings
+              </Link>
+              <Link to={`${ROUTES.profile}/edit`} className={styles.action}>
+                <div className={styles.iconWrapper}>
+                  <FontAwesomeIcon className={styles.icon} icon={faPen} />
+                </div>
+                Change profile
+              </Link>
+              <div
+                onClick={openSettingHandler}
+                className={classNames(
+                  styles.action,
+                  styles.top,
+                  styles.pointer
+                )}
+              >
+                <div className={styles.iconWrapper}>
+                  <FontAwesomeIcon className={styles.icon} icon={faCamera} />
+                </div>
+                Add photos
+              </div>
             </div>
-            <div className={styles.descr}>
-              <div className={styles.name}>{previewUser.name}</div>
-              <div className={styles.age}>{previewUser.age}</div>
-            </div>
-          </Link>
-          <div className={styles.links}>
-            <Link
-              to={ROUTES.settings}
-              className={classNames(styles.link, styles.top)}
-            >
-              <div className={styles.iconWrapper}>
-                <FontAwesomeIcon className={styles.icon} icon={faGear} />
-              </div>
-              Settings
-            </Link>
-            <Link to={`${ROUTES.profile}/edit`} className={styles.link}>
-              <div className={styles.iconWrapper}>
-                <FontAwesomeIcon className={styles.icon} icon={faPen} />
-              </div>
-              Change profile
-            </Link>
-            <Link
-              to={`${ROUTES.profile}/edit`}
-              className={classNames(styles.link, styles.top)}
-            >
-              <div className={styles.iconWrapper}>
-                <FontAwesomeIcon className={styles.icon} icon={faCamera} />
-              </div>
-              Add photos
-            </Link>
           </div>
         </div>
+        <div className={styles.membership}>membership</div>
       </div>
-      <div className={styles.membership}>membership</div>
-    </div>
+      {isDialogUploadOpen && <DialogUpload />}
+      {isImageCropOpen && <CropImage />}
+    </>
   );
 };
