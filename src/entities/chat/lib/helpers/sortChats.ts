1d1d455179a60661974f0b363f0c367edab2f271
Stepasha419a <Dark_Magic419a@mail.ru>
2024-03-05 08:25:04 +0300

    feature: chat created at field implementation

--- src/entities/chat/lib/helpers/sortChats.ts
+++ src/entities/chat/lib/helpers/sortChats.ts
@@ -2,16 +2,27 @@ import type { Chat } from '@/shared/api/interfaces';
 import { getDatesHourDiff } from '@shared/lib/helpers';
 
 export function sortChats(chat1: Chat, chat2: Chat) {
-  if (!chat1.lastMessage?.createdAt) {
-    return -1;
-  } else if (!chat2.lastMessage?.createdAt) {
-    return 1;
+  if (!chat1.lastMessage?.createdAt && chat2.lastMessage?.createdAt) {
+    return getDatesHourDiff(
+      new Date(chat1.createdAt),
+      new Date(chat2.lastMessage.createdAt)
+    ) * -1;
+  } else if (chat1.lastMessage?.createdAt && !chat2.lastMessage?.createdAt) {
+    return getDatesHourDiff(
+      new Date(chat1.lastMessage.createdAt),
+      new Date(chat2.createdAt)
+    ) * -1;
+  } else if (!chat1.lastMessage?.createdAt && !chat2.lastMessage?.createdAt) {
+    return getDatesHourDiff(
+      new Date(chat1.createdAt),
+      new Date(chat2.createdAt)
+    ) * -1;
   }
 
   return (
     getDatesHourDiff(
-      new Date(chat1.lastMessage.createdAt),
-      new Date(chat2.lastMessage.createdAt)
+      new Date(chat1.lastMessage!.createdAt),
+      new Date(chat2.lastMessage!.createdAt)
     ) * -1
   );
 }
--- src/shared/api/interfaces/Chat/Chat.ts
+++ src/shared/api/interfaces/Chat/Chat.ts
@@ -6,6 +6,7 @@ export interface Chat {
   chatVisit?: LastSeen;
   blocked: boolean;
   blockedById?: string;
+  createdAt: string;
 }
 
 export interface LastSeen {
