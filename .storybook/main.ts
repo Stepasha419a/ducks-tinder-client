d56288a71c97c25c067e0509772b949ca92ecc82
Stepan <Dark_Magic419a@mail.ru>
2024-04-12 20:21:58 +0300

    refactor: refactored storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,37 +1,25 @@
-import type { StorybookConfig } from '@storybook/react-webpack5';
-import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
+import { withoutVitePlugins } from '@storybook/builder-vite';
+import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
-    {
-      name: '@storybook/addon-styling',
-      options: {
-        sass: {
-          implementation: require('sass'),
-        },
-      },
-    },
   ],
-  framework: {
-    name: '@storybook/react-webpack5',
-    options: {},
+  core: {
+    builder: '@storybook/builder-vite',
   },
+  framework: '@storybook/react-vite',
   docs: {
     autodocs: 'tag',
   },
-  webpackFinal: async (config) => {
-    config.resolve!.plugins = [
-      ...(config.resolve!.plugins || []),
-      new TsconfigPathsPlugin({
-        extensions: config.resolve!.extensions,
-      }),
-    ];
-    return config;
+  viteFinal: async (config) => {
+    return {
+      ...config,
+      plugins: await withoutVitePlugins(config.plugins, ['node-externals']),
+    };
   },
 };
 export default config;
