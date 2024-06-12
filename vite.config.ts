d6050a1bcaddd539b5cee15f17dd883349ad46ed
Stepan <Dark_Magic419a@mail.ru>
2024-06-12 18:07:45 +0300

    refactor: application is app

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@app': path.resolve(__dirname, './src/application'),
+      '@app': path.resolve(__dirname, './src/app'),
       '@pages': path.resolve(__dirname, './src/pages'),
       '@widgets': path.resolve(__dirname, './src/widgets'),
       '@hooks': path.resolve(__dirname, './src/shared/lib/hooks'),
