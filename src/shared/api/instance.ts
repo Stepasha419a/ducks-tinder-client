fcb962deceaf1d19f545ab9dadbbff2468f6d410
Stepan <Dark_Magic419a@mail.ru>
2024-08-28 11:24:37 +0300

    bugfix: axios interceptors, handle cancel error

--- src/shared/api/instance.ts
+++ src/shared/api/instance.ts
@@ -1,44 +1,8 @@
-import type { AxiosResponse, AxiosError, AxiosRequestConfig } from 'axios';
 import axios from 'axios';
-import type { AuthResponse } from '@shared/api';
+import { setUpInterceptors } from './interceptors';
 
 export const instance = axios.create({
   withCredentials: true,
 });
 
-interface AxiosEditedConfig extends AxiosResponse {
-  _isRetry?: boolean;
-}
-
-instance.interceptors.request.use((config: AxiosRequestConfig) => {
-  config.headers!.Authorization =
-    'Bearer ' + localStorage.getItem('accessToken')!;
-  return config;
-});
-
-instance.interceptors.response.use(
-  (config: AxiosEditedConfig) => config,
-  async (error: AxiosError<{ message: string; status: string }>) => {
-    const originalRequest = error.config as AxiosEditedConfig;
-    if (error.config.url?.endsWith('/auth/refresh')) {
-      return;
-    }
-
-    if (
-      error.response?.status === 401 &&
-      !(error.config as AxiosEditedConfig)._isRetry
-    ) {
-      originalRequest._isRetry = true;
-      const response = await axios.get<AuthResponse>(
-        `${import.meta.env.VITE_AUTH_SERVICE_URL}/auth/refresh`,
-        {
-          withCredentials: true,
-        }
-      );
-      localStorage.setItem('accessToken', response.data.accessToken);
-
-      return instance.request(originalRequest);
-    }
-    throw error;
-  }
-);
+setUpInterceptors(instance);
---
+++ src/shared/api/interceptors.ts
@@ -0,0 +1,46 @@
+import type {
+  AxiosResponse,
+  AxiosRequestConfig,
+  AxiosInstance,
+  AxiosError,
+} from 'axios';
+import axios from 'axios';
+import type { AuthResponse } from '@shared/api';
+
+interface AxiosEditedConfig extends AxiosResponse {
+  _isRetry?: boolean;
+}
+export function setUpInterceptors(instance: AxiosInstance) {
+  instance.interceptors.request.use((config: AxiosRequestConfig) => {
+    config.headers!.Authorization =
+      'Bearer ' + localStorage.getItem('accessToken')!;
+    return config;
+  });
+
+  instance.interceptors.response.use(
+    (config: AxiosEditedConfig) => config,
+    async (error: AxiosError<{ message: string; status: string }>) => {
+      const originalRequest = error.config as AxiosEditedConfig;
+      if (error.config?.url?.endsWith('/auth/refresh')) {
+        return;
+      }
+
+      if (
+        error.response?.status === 401 &&
+        !(error.config as AxiosEditedConfig)._isRetry
+      ) {
+        originalRequest._isRetry = true;
+        const response = await axios.get<AuthResponse>(
+          `${import.meta.env.VITE_AUTH_SERVICE_URL}/auth/refresh`,
+          {
+            withCredentials: true,
+          }
+        );
+        localStorage.setItem('accessToken', response.data.accessToken);
+
+        return instance.request(originalRequest);
+      }
+      throw error;
+    }
+  );
+}
