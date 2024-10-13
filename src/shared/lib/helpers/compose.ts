e7c960047a6d9beeeaefc98b595c1eb50837f601
Stepan <Dark_Magic419a@mail.ru>
2024-10-13 12:45:45 +0300

    refactor: remove compose console log

--- src/shared/lib/helpers/compose.ts
+++ src/shared/lib/helpers/compose.ts
@@ -2,7 +2,6 @@ import type { ComponentType } from 'react';
 import type { HocFunc } from '../interfaces';
 
 export function compose(...funcs: HocFunc[]): HocFunc {
-  console.log(funcs);
   return function composedFunc(arg: ComponentType): ComponentType {
     return funcs.reduceRight((acc: ComponentType, fn: HocFunc) => fn(acc), arg);
   };
