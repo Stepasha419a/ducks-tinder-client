a837fffa539bece3feb3d33d1fdb7a69ab5f540f
Stepan <Dark_Magic419a@mail.ru>
2024-12-08 15:19:32 +0300

    bugfix: fix app-lib cycle deps

--- src/app/lib/hocs/withAppHocs.ts
+++ src/app/lib/hocs/withAppHocs.ts
@@ -1,6 +1,6 @@
-import { WithBrowserRouter } from '@app/lib';
-import { StoreProvider } from '@app/lib';
 import { compose } from '@shared/lib';
 import { WithTheme } from '@shared/lib';
+import { StoreProvider } from '../providers';
+import { WithBrowserRouter } from './BrowserRouter';
 
 export const withAppHocs = compose(WithBrowserRouter, StoreProvider, WithTheme);
