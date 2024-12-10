68dfcb4686efded077689f5a260e995d429fbf6d
Stepasha419a <Dark_Magic419a@mail.ru>
2024-12-10 07:36:38 +0300

    feature: webpack-scripts, custom env config path

--- config/development.config.js
+++ config/development.config.js
@@ -7,124 +7,128 @@ const EslintWebpackPlugin = require('eslint-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-  target: 'web',
-  stats: 'errors-warnings',
-  mode: 'development',
-  devtool: 'inline-source-map',
-  entry: './src/index.tsx',
-  output: {
-    filename: 'js/[name].bundle.js',
-    chunkFilename: 'js/[name].chunk.js',
-    path: path.resolve(__dirname, '..', 'dist'),
-    assetModuleFilename: 'media/[name].[hash][ext]',
-    publicPath: '/',
-    clean: true,
-  },
-  cache: {
-    type: 'memory',
-    cacheUnaffected: true,
-  },
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, '..', 'dist'),
+module.exports = (env) => {
+  const envPath = env.envPath || '.env';
+
+  return {
+    target: 'web',
+    stats: 'errors-warnings',
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: './src/index.tsx',
+    output: {
+      filename: 'js/[name].bundle.js',
+      chunkFilename: 'js/[name].chunk.js',
+      path: path.resolve(__dirname, '..', 'dist'),
+      assetModuleFilename: 'media/[name].[hash][ext]',
+      publicPath: '/',
+      clean: true,
     },
-    compress: true,
-    port: 3000,
-    hot: true,
-    historyApiFallback: true,
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    plugins: [new TsconfigPathsPlugin()],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(sass|less|scss|css)$/i,
-        exclude: /\.module\.scss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+    cache: {
+      type: 'memory',
+      cacheUnaffected: true,
+    },
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, '..', 'dist'),
       },
-      {
-        test: /\.module\.(sass|less|scss|css)$/i,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                localIdentName: '[name]__[local]--[hash:base64:5]',
+      compress: true,
+      port: 3000,
+      hot: true,
+      historyApiFallback: true,
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+      plugins: [new TsconfigPathsPlugin()],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(sass|less|scss|css)$/i,
+          exclude: /\.module\.scss$/i,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.module\.(sass|less|scss|css)$/i,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  localIdentName: '[name]__[local]--[hash:base64:5]',
+                },
               },
             },
-          },
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/images/[name].[hash][ext]',
+            'sass-loader',
+          ],
         },
-      },
-      {
-        test: /\.(ttf|woff|woff2|eot|otf)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/fonts/[name][ext]',
-        },
-      },
-      {
-        test: /.tsx?$/i,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            cacheDirectory: true,
-            cacheCompression: false,
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/images/[name].[hash][ext]',
           },
         },
-        exclude: /node_modules/,
-      },
+        {
+          test: /\.(ttf|woff|woff2|eot|otf)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/fonts/[name][ext]',
+          },
+        },
+        {
+          test: /.tsx?$/i,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+              cacheCompression: false,
+            },
+          },
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebPlugin({
+        template: './index.html',
+      }),
+      new FaviconsWebpackPlugin({
+        logo: './public/favicon.png',
+        manifest: './public/manifest.json',
+      }),
+      new ReactRefreshWebpackPlugin({
+        overlay: false,
+      }),
+      new ForkTsCheckerWebpackPlugin(),
+      new EslintWebpackPlugin({
+        context: '../',
+        emitWarning: true,
+        failOnError: false,
+        extensions: ['ts', 'tsx'],
+        overrideConfigFile: './.eslintrc.cjs',
+      }),
+      new Dotenv({
+        path: envPath,
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebPlugin({
-      template: './index.html',
-    }),
-    new FaviconsWebpackPlugin({
-      logo: './public/favicon.png',
-      manifest: './public/manifest.json',
-    }),
-    new ReactRefreshWebpackPlugin({
-      overlay: false,
-    }),
-    new ForkTsCheckerWebpackPlugin(),
-    new EslintWebpackPlugin({
-      context: '../',
-      emitWarning: true,
-      failOnError: false,
-      extensions: ['ts', 'tsx'],
-      overrideConfigFile: './.eslintrc.cjs',
-    }),
-    new Dotenv({
-      path: '.env',
-    }),
-  ],
-  optimization: {
-    usedExports: true,
-    moduleIds: 'deterministic',
-    runtimeChunk: 'single',
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
+    optimization: {
+      usedExports: true,
+      moduleIds: 'deterministic',
+      runtimeChunk: 'single',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
         },
       },
     },
-  },
-  watchOptions: {
-    ignored: /node_modules/,
-  },
+    watchOptions: {
+      ignored: /node_modules/,
+    },
+  };
 };
--- config/production.config.js
+++ config/production.config.js
@@ -7,106 +7,110 @@ const Dotenv = require('dotenv-webpack');
 /* const BundleAnalyzerPlugin =
   require('webpack-bundle-analyzer').BundleAnalyzerPlugin; */
 
-module.exports = {
-  target: 'web',
-  stats: 'errors-warnings',
-  mode: 'development',
-  devtool: 'inline-source-map',
-  entry: './src/index.tsx',
-  output: {
-    filename: 'js/[name].bundle.js',
-    chunkFilename: 'js/[name].chunk.js',
-    path: path.resolve(__dirname, '..', 'dist'),
-    assetModuleFilename: 'media/[name].[hash][ext]',
-    publicPath: '/',
-    clean: true,
-  },
-  cache: {
-    type: 'memory',
-    cacheUnaffected: true,
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    plugins: [new TsconfigPathsPlugin()],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(sass|less|scss|css)$/i,
-        exclude: /\.module\.scss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.module\.(sass|less|scss|css)$/i,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                localIdentName: '[name]__[local]--[hash:base64:5]',
+module.exports = (env) => {
+  const envPath = env.envPath || '.env';
+
+  return {
+    target: 'web',
+    stats: 'errors-warnings',
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: './src/index.tsx',
+    output: {
+      filename: 'js/[name].bundle.js',
+      chunkFilename: 'js/[name].chunk.js',
+      path: path.resolve(__dirname, '..', 'dist'),
+      assetModuleFilename: 'media/[name].[hash][ext]',
+      publicPath: '/',
+      clean: true,
+    },
+    cache: {
+      type: 'memory',
+      cacheUnaffected: true,
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+      plugins: [new TsconfigPathsPlugin()],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(sass|less|scss|css)$/i,
+          exclude: /\.module\.scss$/i,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.module\.(sass|less|scss|css)$/i,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  localIdentName: '[name]__[local]--[hash:base64:5]',
+                },
               },
             },
-          },
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/images/[name].[hash][ext]',
+            'sass-loader',
+          ],
         },
-      },
-      {
-        test: /\.(ttf|woff|woff2|eot|otf)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/fonts/[name][ext]',
-        },
-      },
-      {
-        test: /.tsx?$/i,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            cacheDirectory: true,
-            cacheCompression: false,
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/images/[name].[hash][ext]',
           },
         },
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebPlugin({
-      template: './index.html',
-    }),
-    new FaviconsWebpackPlugin({
-      logo: './public/favicon.png',
-      manifest: './public/manifest.json',
-    }),
-    new Dotenv({
-      path: '.env',
-    }),
-    //new BundleAnalyzerPlugin(),
-  ],
-  optimization: {
-    moduleIds: 'deterministic',
-    runtimeChunk: 'multiple',
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
+        {
+          test: /\.(ttf|woff|woff2|eot|otf)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/fonts/[name][ext]',
+          },
         },
-      },
+        {
+          test: /.tsx?$/i,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+              cacheCompression: false,
+            },
+          },
+          exclude: /node_modules/,
+        },
+      ],
     },
-    minimize: true,
-    minimizer: [new TerserPlugin()],
-  },
-  watchOptions: {
-    ignored: /node_modules/,
-  },
+    plugins: [
+      new HtmlWebPlugin({
+        template: './index.html',
+      }),
+      new FaviconsWebpackPlugin({
+        logo: './public/favicon.png',
+        manifest: './public/manifest.json',
+      }),
+      new Dotenv({
+        path: envPath,
+      }),
+      //new BundleAnalyzerPlugin(),
+    ],
+    optimization: {
+      moduleIds: 'deterministic',
+      runtimeChunk: 'multiple',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
+        },
+      },
+      minimize: true,
+      minimizer: [new TerserPlugin()],
+    },
+    watchOptions: {
+      ignored: /node_modules/,
+    },
+  };
 };
