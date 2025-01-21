020f7b19b35dbb4d307d8e2aa53602d16c0c0d16
stepan <dark_magic419a@mail.ru>
2025-01-21 20:39:14 +0300

    refactor: ui-app ts vite-config

---
+++ apps/ui/vite.config.ts
@@ -0,0 +1,49 @@
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
+import checker from 'vite-plugin-checker';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: '/',
+    publicDir: 'public',
+    logLevel: 'warn',
+
+    plugins: [
+      react({ include: '**/*.tsx' }),
+      checker({
+        typescript: true,
+        eslint: {
+          // for example, lint .ts and .tsx
+          lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+        },
+      }),
+    ],
+    preview: {
+      port: 3000,
+    },
+    server: {
+      port: 3000,
+    },
+    resolve: {
+      alias: {
+        '@app': path.resolve(__dirname, './src/app'),
+        '@pages': path.resolve(__dirname, './src/pages'),
+        '@pages/*': path.resolve(__dirname, './src/pages'),
+        '@widgets': path.resolve(__dirname, './src/widgets/index'),
+        '@widgets/*': path.resolve(__dirname, './src/widgets'),
+        '@hooks': path.resolve(__dirname, './src/shared/lib/hooks'),
+        '@features': path.resolve(__dirname, './src/features'),
+        '@entities': path.resolve(__dirname, './src/entities'),
+        '@shared/constants': path.resolve(
+          __dirname,
+          './src/shared/lib/constants'
+        ),
+        '@shared/helpers': path.resolve(__dirname, './src/shared/lib/helpers'),
+        '@shared': path.resolve(__dirname, './src/shared'),
+      },
+    },
+  };
+});
