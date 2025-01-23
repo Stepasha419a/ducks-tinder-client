3ddba63b6bfb304346cc01503221bfe3c354eb04
stepan <dark_magic419a@mail.ru>
2025-01-23 20:21:08 +0300

    refactor: ui-app generate build dts

--- apps/ui/vite.config.ts
+++ apps/ui/vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 import checker from 'vite-plugin-checker';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -23,6 +24,7 @@ export default defineConfig(({ mode }) => {
           lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
         },
       }),
+      dts({ rollupTypes: true, exclude: ['**/*.stories.tsx'] }),
     ],
     preview: {
       port: 3000,
