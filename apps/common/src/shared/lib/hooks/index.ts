7c6d6289826469414c7c8d582cdb060478bc5776
stepan <dark_magic419a@mail.ru>
2025-01-30 20:58:30 +0300

    refactor: common-app remove redux-hooks

--- apps/common/src/shared/lib/hooks/index.ts
+++ apps/common/src/shared/lib/hooks/index.ts
@@ -1,5 +1,3 @@
-export { useAppDispatch } from './useAppDispatch';
-export { useAppSelector } from './useAppSelector';
 export { useScrollToBottom } from './useScrollToBottom';
 export { useIntersectionObserver } from './useIntersectionObserver';
 export { useDebouncedCallback } from './useDebouncedCallback';
