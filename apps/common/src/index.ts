3fe0de86cd7ea329ad06e194e7d40502638937be
stepan <dark_magic419a@mail.ru>
2025-03-30 15:53:33 +0300

    feature: common-app re-export user-entity

--- apps/common/src/index.ts
+++ apps/common/src/index.ts
@@ -2,6 +2,7 @@ export * from './shared/lib';
 export * from './shared/api';
 
 export * from './entities/chat';
+export * from './entities/user';
 
 export * from './app/lib';
 export * from './app/model';
