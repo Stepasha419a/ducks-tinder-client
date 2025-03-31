50e1f33f138f96e4cc6a1057cabe6e06c065d03e
stepan <dark_magic419a@mail.ru>
2025-03-31 09:03:29 +0300

    refactor: common-app root-reducer auth, user slices

--- apps/common/src/app/model/rootReducer.ts
+++ apps/common/src/app/model/rootReducer.ts
@@ -1,6 +1,9 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { chatReducer } from '@entities/chat';
+import { authReducer, userReducer } from '@entities/user';
 
 export const rootReducer = combineReducers({
   chat: chatReducer,
+  auth: authReducer,
+  user: userReducer,
 });
