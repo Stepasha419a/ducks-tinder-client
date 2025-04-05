d831d3bb11ebf1e7fc1b10c7d23ce49b253fe0c1
stepan <dark_magic419a@mail.ru>
2025-04-05 20:58:37 +0300

    feature: root-app use user-reducer from common-package

--- apps/root/src/app/model/rootReducer.ts
+++ apps/root/src/app/model/rootReducer.ts
@@ -1,8 +1,8 @@
 import { chatReducer } from '@ducks-tinder-client/common';
 import { authReducer } from '@ducks-tinder-client/common';
+import { userReducer } from '@ducks-tinder-client/common';
 import { combineReducers } from '@reduxjs/toolkit';
 import { tinderReducer } from '@entities/user';
-import { userReducer } from '@entities/user';
 import { pairReducer } from '@entities/user';
 
 export const rootReducer = combineReducers({
