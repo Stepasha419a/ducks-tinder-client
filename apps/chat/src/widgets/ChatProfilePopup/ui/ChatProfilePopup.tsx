4f87c2713d96cecf5eaa1cad4acdfc9f49aafd54
stepan <dark_magic419a@mail.ru>
2025-04-07 07:43:32 +0300

    feature: chat-app widgets

---
+++ apps/chat/src/widgets/ChatProfilePopup/ui/ChatProfilePopup.tsx
@@ -0,0 +1,33 @@
+import type { FC } from 'react';
+
+import { useAppSelector } from '@ducks-tinder-client/common';
+import { Popup, Preview } from '@ducks-tinder-client/ui';
+
+import { ChatControl } from '@features/ChatControl';
+
+import { ChatProfilePopupLazy } from './ChatProfilePopup.lazy';
+import styles from './ChatProfilePopup.module.scss';
+
+interface ChatProfilePopupProps {
+  handleClose: () => void;
+}
+
+export const ChatProfilePopup: FC<ChatProfilePopupProps> = ({
+  handleClose,
+}) => {
+  const chatMember = useAppSelector((state) => state.chat.chatMember);
+
+  return (
+    <Popup closeHandler={handleClose} size="l" extraClassName={styles.popup}>
+      {chatMember ? (
+        <Preview
+          user={chatMember}
+          isFull
+          extraContent={<ChatControl submitDelete={handleClose} />}
+        />
+      ) : (
+        <ChatProfilePopupLazy />
+      )}
+    </Popup>
+  );
+};
