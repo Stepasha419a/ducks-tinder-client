397c23e1440d2fb8ec0ba32099803eb3572c3d4b
stepan <dark_magic419a@mail.ru>
2025-04-11 22:04:37 +0300

    feature: root-app chat-app remote-declarations

--- apps/root/src/app/lib/types/remote_declarations.d.ts
+++ apps/root/src/app/lib/types/remote_declarations.d.ts
@@ -4,3 +4,24 @@ declare module 'policyApp/Policy' {
   const component: PolicyAppDeclaration;
   export default component;
 }
+
+declare module 'chatApp/ActiveChat' {
+  import type { ChatAppActiveChat } from '@ducks-tinder-client/common';
+
+  const component: ChatAppActiveChat;
+  export default component;
+}
+
+declare module 'chatApp/IndexChat' {
+  import type { ChatAppIndexChat } from '@ducks-tinder-client/common';
+
+  const component: ChatAppIndexChat;
+  export default component;
+}
+
+declare module 'chatApp/chat' {
+  import type { ChatAppComponentsDeclaration } from '@ducks-tinder-client/common';
+
+  export const ChatList: ChatAppComponentsDeclaration['ChatList'];
+  export const ChatTabTitle: ChatAppComponentsDeclaration['ChatTabTitle'];
+}
