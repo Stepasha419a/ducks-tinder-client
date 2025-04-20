756fa6da5a973ce9a0565f617040d403f405db0a
stepan <dark_magic419a@mail.ru>
2025-04-20 12:20:25 +0300

    refactor: root-app ui-package global styles

--- apps/root/src/app/index.tsx
+++ apps/root/src/app/index.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider, ToastContainer } from '@ducks-tinder-client/ui';
 import { withAppHocs } from './lib';
 import { Routing } from './routing';
 import './styles/index.scss';
-import '@ducks-tinder-client/ui/dist/ui.css';
+import '@ducks-tinder-client/ui/dist/esm/index.css';
 
 // TODO: decompose ThemeProvider into hoc
 function App(): ReactElement {
