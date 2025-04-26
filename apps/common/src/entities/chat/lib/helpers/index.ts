42676796252d66d607c6611783b37f27320a56e6
stepan <dark_magic419a@mail.ru>
2025-04-26 22:09:36 +0300

    feature: common-app chat-enitity helpers

---
+++ apps/common/src/entities/chat/lib/helpers/index.ts
@@ -0,0 +1,2 @@
+export { getIsChatPage } from './getIsChatPage';
+export { getIsActiveChatPage } from './getIsActiveChatPage';
---
+++ apps/common/src/entities/chat/lib/index.ts
@@ -0,0 +1,1 @@
+export { getIsActiveChatPage, getIsChatPage } from './helpers';
