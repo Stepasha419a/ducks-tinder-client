26117ee1f830fc6bfdb2d2b6f50a8fb2aaf9eae2
Stepasha419a <Dark_Magic419a@mail.ru>
2025-04-30 07:23:08 +0300

    feature: root-app use hocs from common-app

--- apps/root/src/app/lib/hocs/index.ts
+++ apps/root/src/app/lib/hocs/index.ts
@@ -1,10 +1,12 @@
-import { WithErrorFallback } from '@ducks-tinder-client/common';
+import {
+  WithChatConnection,
+  WithErrorFallback,
+  WithNewMessagesCount,
+} from '@ducks-tinder-client/common';
 
 import { WithAuthRedirect } from '@features/WithAuthRedirect';
-import { WithChatConnection } from '@features/WithChatConnection';
 import { WithCheckedFields } from '@features/WithCheckedFields';
 import { WithUserData } from '@features/WithUserData';
-import { WithNewMessagesCount } from '@entities/chat';
 import { WithInitialLoading, WithPairsInfo } from '@entities/user';
 import { authHocComposition, privateHocComposition } from '@shared/lib';
 
