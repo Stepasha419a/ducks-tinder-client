c69a395a0a290de5ba0ada618a10859f6baca7c9
Stepasha419a <Dark_Magic419a@mail.ru>
2025-05-05 21:08:15 +0300

    refactor: root-app remove shared-state hooks

--- apps/root/src/shared/lib/hooks/index.ts
+++
@@ -1,2 +0,0 @@
-export { useAppDispatch } from './useAppDispatch';
-export { useAppSelector } from './useAppSelector';
--- apps/root/src/shared/lib/hooks/useAppSelector.ts
+++
@@ -1,4 +0,0 @@
-import type { TypedUseSelectorHook } from 'react-redux';
-import { useSelector } from 'react-redux';
-
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
