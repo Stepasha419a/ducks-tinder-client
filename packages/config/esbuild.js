50f4dff34c293f4e9f77023f8a7ad38e97f92555
stepan <dark_magic419a@mail.ru>
2025-05-09 22:49:57 +0300

    refactor: config-package esbuild concurrent build

--- packages/config/esbuild.js
+++ packages/config/esbuild.js
@@ -1,26 +1,27 @@
 import * as esbuild from 'esbuild';
 import { nodeExternalsPlugin } from 'esbuild-node-externals';
 
-await esbuild.build({
-  entryPoints: ['./src/index.ts'],
-  outfile: 'dist/cjs/index.js',
-  bundle: true,
-  minify: true,
-  treeShaking: true,
-  platform: 'node',
-  format: 'cjs',
-  target: 'esnext',
-  plugins: [nodeExternalsPlugin()],
-});
+const build = (format) =>
+  esbuild.build({
+    entryPoints: ['./src/index.ts'],
+    outdir: `dist/${format}`,
+    bundle: true,
+    minify: true,
+    treeShaking: true,
+    platform: 'node',
+    format,
+    target: 'esnext',
+    plugins: [nodeExternalsPlugin()],
+  });
 
-await esbuild.build({
-  entryPoints: ['./src/index.ts'],
-  outfile: 'dist/esm/index.js',
-  bundle: true,
-  minify: true,
-  treeShaking: true,
-  platform: 'node',
-  format: 'esm',
-  target: 'esnext',
-  plugins: [nodeExternalsPlugin()],
-});
+build('cjs')
+  .then(async () => {
+    console.log('CJS Build complete');
+  })
+  .catch(() => process.exit(1));
+
+build('esm')
+  .then(async () => {
+    console.log('ESM Build complete');
+  })
+  .catch(() => process.exit(1));
