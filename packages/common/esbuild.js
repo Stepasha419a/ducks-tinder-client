a71c7ff9912176e0dc9ae8dbae66f31a3ff74db4
stepan <dark_magic419a@mail.ru>
2025-06-01 20:23:04 +0300

    refactor: common-package format esbuild config

--- packages/common/esbuild.js
+++ packages/common/esbuild.js
@@ -1,33 +1,33 @@
 import * as esbuild from 'esbuild';
 import { nodeExternalsPlugin } from 'esbuild-node-externals';
 
-import packageData from './package.json' with { type: "json" };
+import packageData from './package.json' with { type: 'json' };
 
 const build = (format) =>
   esbuild.build({
-  entryPoints: ['./src/index.ts'],
-  outdir: `dist/${format}`,
-  bundle: true,
-  minify: true,
-  treeShaking: true,
-  platform: 'browser',
-  format,
-  target: 'esnext',
-  plugins: [nodeExternalsPlugin()],
-  external: [].concat(
-    Object.keys(packageData.dependencies),
-    Object.keys(packageData.peerDependencies)
-  ),
-});
+    entryPoints: ['./src/index.ts'],
+    outdir: `dist/${format}`,
+    bundle: true,
+    minify: true,
+    treeShaking: true,
+    platform: 'browser',
+    format,
+    target: 'esnext',
+    plugins: [nodeExternalsPlugin()],
+    external: [].concat(
+      Object.keys(packageData.dependencies),
+      Object.keys(packageData.peerDependencies)
+    ),
+  });
 
 build('cjs')
   .then(async () => {
-    console.log('CJS Build complete');
+    globalThis.console.log('CJS Build complete');
   })
-  .catch(() => process.exit(1));
+  .catch(() => globalThis.process.exit(1));
 
 build('esm')
   .then(async () => {
-    console.log('ESM Build complete');
+    globalThis.console.log('ESM Build complete');
   })
-  .catch(() => process.exit(1));
+  .catch(() => globalThis.process.exit(1));
