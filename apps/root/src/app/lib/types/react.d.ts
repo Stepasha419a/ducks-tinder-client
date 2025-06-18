84dfacea015d9f0ec71a7f69e5fa8ddea2740d77
stepan <dark_magic419a@mail.ru>
2025-06-18 08:48:12 +0300

    refactor: root-app change styles export types

--- apps/root/src/app/lib/types/react.d.ts
+++ apps/root/src/app/lib/types/react.d.ts
@@ -56,15 +56,15 @@ declare module '*.svg' {
 
 declare module '*.module.css' {
   const classes: { readonly [key: string]: string };
-  export default classes;
+  export = classes;
 }
 
 declare module '*.module.scss' {
   const classes: { readonly [key: string]: string };
-  export default classes;
+  export = classes;
 }
 
 declare module '*.module.sass' {
   const classes: { readonly [key: string]: string };
-  export default classes;
+  export = classes;
 }
