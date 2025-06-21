c33a61997cfaa74acde08772b88d3da6bc6ddc28
stepan <dark_magic419a@mail.ru>
2025-06-21 11:13:47 +0300

    refactor: root-app fix lint

--- apps/root/src/features/SwipeUser/SwipeUser.tsx
+++ apps/root/src/features/SwipeUser/SwipeUser.tsx
@@ -53,9 +53,6 @@ export const SwipeUser: FC = () => {
       {tinderUsers[0] && (
         <motion.div className={styles.swipeUser} {...motionProps}>
           {!previewProps.isFull && <Status {...statusProps} />}
-          {/* TODO: update ui-package to fix types */}
-          {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
-          {/* @ts-expect-error */}
           <Preview
             user={tinderUsers[0]}
             extraClassName={classNames(
