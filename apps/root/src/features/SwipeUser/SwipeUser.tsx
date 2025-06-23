695cdbc599ee777a37e391f1787cd13683d2a9b5
stepan <dark_magic419a@mail.ru>
2025-06-23 21:50:05 +0300

    feature: root-app swipe-user simplify just for one card motion

--- apps/root/src/features/SwipeUser/SwipeUser.tsx
+++ apps/root/src/features/SwipeUser/SwipeUser.tsx
@@ -1,70 +1,64 @@
-import type { FC } from 'react';
-import { useEffect } from 'react';
+import type { Dispatch, FC, SetStateAction } from 'react';
 import classNames from 'classnames';
 import { motion } from 'motion/react';
 
-import {
-  useAdaptiveMediaQuery,
-  useAppDispatch,
-  useAppSelector,
-} from '@ducks-tinder-client/common';
+import type { ShortUser } from '@ducks-tinder-client/common';
+import { useAdaptiveMediaQuery } from '@ducks-tinder-client/common';
 import { Preview } from '@ducks-tinder-client/ui';
 
-import { getMatchUsersThunk, selectTinderData } from '@entities/user';
-
 import { useSwipe } from './lib';
-import { SwipeUserLazy } from './SwipeUser.lazy';
-import { Failed, RateButtons, Status } from './ui';
+import { Status } from './ui';
 import * as styles from './SwipeUser.module.scss';
+import type { TinderAnimations } from '@entities/user';
 
-export const SwipeUser: FC = () => {
-  const dispatch = useAppDispatch();
+interface Props {
+  user: ShortUser;
+  onChangeX?: (value: number, userId: string) => void;
+  onChangeY?: (value: number, userId: string) => void;
+  isFullPreview: boolean;
+  setIsFullPreview: Dispatch<SetStateAction<boolean>>;
+  disabled?: boolean;
+  overriddenAnimation: {
+    userId: string;
+    animation: TinderAnimations;
+  } | null;
+  onSubmit: () => void;
+}
 
+export const SwipeUser: FC<Props> = ({
+  user,
+  disabled,
+  onChangeX,
+  onChangeY,
+  isFullPreview,
+  setIsFullPreview,
+  onSubmit,
+  overriddenAnimation,
+}) => {
   const isMobile = useAdaptiveMediaQuery('(max-width: 900px)');
 
-  const { tinderUsers, tinderUsersLength, isFailed, isReturnLoading } =
-    useAppSelector(selectTinderData);
-
-  const { motionProps, statusProps, previewProps, rateButtonsProps } =
-    useSwipe();
-
-  useEffect(() => {
-    if (tinderUsersLength < 3) {
-      dispatch(getMatchUsersThunk());
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [dispatch]);
-
-  if (isFailed) {
-    return <Failed />;
-  }
+  const { motionProps, statusProps, previewProps } = useSwipe({
+    isFullPreview,
+    setIsFullPreview,
+    disabled,
+    onChangeX,
+    onChangeY,
+    userId: user.id,
+    onSubmit,
+    overriddenAnimation,
+  });
 
   return (
-    <div className={styles.usersContainer}>
-      <SwipeUserLazy small />
-      {tinderUsers[1] && (
-        <Preview
-          user={tinderUsers[1]}
-          extraClassName={classNames(styles.pending, isMobile && styles.mobile)}
-          isShadow
-          disabled
-        />
-      )}
-      {tinderUsers[0] && (
-        <motion.div className={styles.swipeUser} {...motionProps}>
-          {!previewProps.isFull && <Status {...statusProps} />}
-          <Preview
-            user={tinderUsers[0]}
-            extraClassName={classNames(
-              previewProps.isFull ? styles.padding : styles.grabbing,
-              isMobile && styles.mobile
-            )}
-            {...previewProps}
-          />
-        </motion.div>
-      )}
-      {isReturnLoading && <SwipeUserLazy />}
-      <RateButtons {...rateButtonsProps} />
-    </div>
+    <motion.div className={styles.swipeUser} {...motionProps}>
+      {!previewProps.isFull && <Status {...statusProps} />}
+      <Preview
+        user={user}
+        extraClassName={classNames(
+          previewProps.isFull ? styles.padding : styles.grabbing,
+          isMobile && styles.mobile
+        )}
+        {...previewProps}
+      />
+    </motion.div>
   );
 };
