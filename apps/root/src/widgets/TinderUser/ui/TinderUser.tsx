59e8a7444ab560a330c9ddceed2d3e5c60a14baf
stepan <dark_magic419a@mail.ru>
2025-06-26 09:10:45 +0300

    feature: root-app tinder-user render many swipe-users

--- apps/root/src/widgets/TinderUser/ui/TinderUser.tsx
+++ apps/root/src/widgets/TinderUser/ui/TinderUser.tsx
@@ -3,10 +3,12 @@ import classNames from 'classnames';
 
 import { useAdaptiveMediaQuery } from '@ducks-tinder-client/common';
 
-import { SwipeUser } from '@features/SwipeUser';
 import { Explore } from '@entities/user';
 
 import * as styles from './TinderUser.module.scss';
+import { SwipeUser } from '@features/SwipeUser';
+import { RateButtons } from '@features/RateButtons';
+import { useAnimationActions } from '../lib';
 
 interface TinderUserProps {
   explore?: boolean;
@@ -14,6 +16,19 @@ interface TinderUserProps {
 
 export const TinderUser: FC<TinderUserProps> = ({ explore }) => {
   const isMobile = useAdaptiveMediaQuery('(max-width: 900px)');
+
+  const {
+    isFullPreview,
+    onAnimation,
+    onChangeX,
+    onChangeY,
+    onSubmit,
+    overriddenAnimation,
+    setIsFullPreview,
+    tinderUsers,
+    x,
+    y,
+  } = useAnimationActions();
 
   return (
     <div
@@ -25,7 +40,23 @@ export const TinderUser: FC<TinderUserProps> = ({ explore }) => {
     >
       {explore && <Explore />}
       <div className={styles.users}>
-        <SwipeUser />
+        {tinderUsers.toReversed().map((user, index) => {
+          const isActive = index === tinderUsers.length - 1;
+
+          return (
+            <SwipeUser
+              overriddenAnimation={overriddenAnimation}
+              key={user.id}
+              user={user}
+              disabled={!isActive}
+              isFullPreview={isFullPreview}
+              setIsFullPreview={setIsFullPreview}
+              onChangeX={isActive ? onChangeX : undefined}
+              onChangeY={isActive ? onChangeY : undefined}
+              onSubmit={onSubmit}
+            />
+          );
+        })}
       </div>
     </div>
   );
