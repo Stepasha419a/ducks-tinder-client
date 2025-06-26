e8dd226872eea94c3c6c05d29fc9afc42e6fe2d4
stepan <dark_magic419a@mail.ru>
2025-06-26 09:13:49 +0300

    feature: root-app tinder-user add loading ui

--- apps/root/src/widgets/TinderUser/ui/TinderUser.tsx
+++ apps/root/src/widgets/TinderUser/ui/TinderUser.tsx
@@ -1,12 +1,15 @@
 import type { FC } from 'react';
 import classNames from 'classnames';
 
-import { useAdaptiveMediaQuery } from '@ducks-tinder-client/common';
+import {
+  useAdaptiveMediaQuery,
+  useAppSelector,
+} from '@ducks-tinder-client/common';
 
 import { Explore } from '@entities/user';
 
 import * as styles from './TinderUser.module.scss';
-import { SwipeUser } from '@features/SwipeUser';
+import { SwipeUser, SwipeUserLazy } from '@features/SwipeUser';
 import { RateButtons } from '@features/RateButtons';
 import { useAnimationActions } from '../lib';
 
@@ -16,6 +19,10 @@ interface TinderUserProps {
 
 export const TinderUser: FC<TinderUserProps> = ({ explore }) => {
   const isMobile = useAdaptiveMediaQuery('(max-width: 900px)');
+
+  const isReturnLoading = useAppSelector(
+    (state) => state.tinder.isReturnLoading
+  );
 
   const {
     isFullPreview,
@@ -40,6 +47,7 @@ export const TinderUser: FC<TinderUserProps> = ({ explore }) => {
     >
       {explore && <Explore />}
       <div className={styles.users}>
+        <SwipeUserLazy key="loading" />
         {tinderUsers.toReversed().map((user, index) => {
           const isActive = index === tinderUsers.length - 1;
 
@@ -57,6 +65,7 @@ export const TinderUser: FC<TinderUserProps> = ({ explore }) => {
             />
           );
         })}
+        {isReturnLoading && <SwipeUserLazy key="return-loading" />}
         <RateButtons
           key="rate-buttons"
           onAnimation={onAnimation}
