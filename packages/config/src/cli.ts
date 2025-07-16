c48e78a78318948f974601ba8721e72ff7dd9bb8
stepan <dark_magic419a@mail.ru>
2025-07-16 21:44:08 +0300

    feature: config-package add apply-env command

--- packages/config/src/cli.ts
+++ packages/config/src/cli.ts
@@ -5,3 +5,15 @@ import { Command, Option } from 'commander';
 const program = new Command();
 
 program.name('dtc').description('Ducks Tinder Client CLI').version('1.0.0');
+
+program
+  .command('apply-env')
+  .description('Apply env from env-file to browser window')
+  .addOption(
+    new Option('-m, --mode <mode>', 'Env mode')
+      .choices([EnvModes.Dev, EnvModes.Prod, EnvModes.Demo])
+      .default(EnvModes.Dev)
+  )
+  .action((options) => {
+    applyEnv(options.mode);
+  });
