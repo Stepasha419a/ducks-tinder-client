aeb72c7c8caad764049a4474bf1d52f1a2f07110
stepan <dark_magic419a@mail.ru>
2025-07-16 20:22:51 +0300

    feature: config-package apply-env script

---
+++ packages/config/src/apply-env.ts
@@ -0,0 +1,41 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+enum Modes {
+  Dev = 'dev',
+  Demo = 'demo',
+  Prod = 'prod',
+}
+
+export function applyEnv(mode: Modes) {
+  console.log(`Env mode: ${mode}`);
+
+  const envPath = path.resolve(__dirname, path.join('..', `.env.${mode}`));
+
+  const envFile = fs.readFileSync(envPath, 'utf-8');
+
+  const lines = envFile
+    .split('\n')
+    .filter((line) => line.trim() !== '' && !line.trim().startsWith('#'));
+
+  const result: Record<string, string> = {};
+
+  for (const line of lines) {
+    const [key, ...rest] = line.split('=');
+    const envKey = key.trim();
+    const envValueFromEnv = process.env[envKey];
+    const envValueFromFile = rest.join('=').trim().replace(/^"|"$/g, '');
+
+    result[envKey] = envValueFromEnv || envValueFromFile;
+  }
+
+  const output = [
+    'window._env_ = {',
+    ...Object.entries(result).map(([key, value]) => `  ${key}: "${value}",`),
+    '};',
+  ].join('\n');
+
+  fs.writeFileSync(path.resolve(__dirname, '../env-config.js'), output);
+
+  console.log('Create env-config.js');
+}
