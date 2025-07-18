f63e07b768117ac60bc67c4141f32a3568a332ed
stepan <dark_magic419a@mail.ru>
2025-07-18 20:43:57 +0300

    feature: common-package lib-settings constant

--- packages/common/src/shared/lib/constants/index.ts
+++ packages/common/src/shared/lib/constants/index.ts
@@ -2,3 +2,4 @@ export { EMAIL_REGEXP } from './emailRegExp';
 export { HOUR_IN_MS } from './hourInMs';
 export { PAGINATION_TAKE } from './paginationTake';
 export { ROUTES } from './routes';
+export { LIB_SETTINGS } from './libSettings';
