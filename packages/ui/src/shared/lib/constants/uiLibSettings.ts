bdb7a18881f5217e06aa02019823d887d3f5d726
stepan <dark_magic419a@mail.ru>
2025-07-18 23:25:23 +0300

    feature: ui-package ui-lib-settings

---
+++ packages/ui/src/shared/lib/constants/uiLibSettings.ts
@@ -0,0 +1,7 @@
+export interface UILibSettings {
+  IMAGE_BASE_URL: string;
+}
+
+export const UI_LIB_SETTINGS: UILibSettings = {
+  IMAGE_BASE_URL: '',
+};
--- packages/ui/src/shared/lib/index.ts
+++ packages/ui/src/shared/lib/index.ts
@@ -1,2 +1,3 @@
 export * from './helpers';
 export * from './providers';
+export * from './constants';
