c500a45576b5a3974db70db7769f0cb0dac31c66
stepan <dark_magic419a@mail.ru>
2025-07-20 16:40:44 +0300

    feature: common-package service-getter

--- packages/common/src/shared/api/services/index.ts
+++ packages/common/src/shared/api/services/index.ts
@@ -2,10 +2,42 @@ export * from './helper';
 
 export * from './mock';
 
-export { authService } from './auth/auth.service';
-export { chatService } from './chat/chat.service';
-export { userService } from './user/user.service';
-
 export * from './chat/chat-service.interface';
 export * from './user/user-service.interface';
 export * from './auth/auth.interfaces';
+
+import { AuthService } from './auth/auth.interfaces';
+import { createAuthService } from './auth/auth.service';
+import { ChatService } from './chat/chat-service.interface';
+import { createChatService } from './chat/chat.service';
+import { UserService } from './user/user-service.interface';
+import { createUserService } from './user/user.service';
+
+interface Instances {
+  authService: AuthService | null;
+  chatService: ChatService | null;
+  userService: UserService | null;
+}
+
+const instances: Instances = {
+  authService: null,
+  chatService: null,
+  userService: null,
+};
+
+const getOrCreateService = <T extends Instances[keyof Instances]>(
+  key: keyof Instances,
+  createService: () => T
+): T => {
+  if (!instances[key]) {
+    instances[key] = createService() as AuthService & ChatService & UserService;
+  }
+
+  return instances[key] as T;
+};
+
+export const serviceGetter = {
+  getAuthService: () => getOrCreateService('authService', createAuthService),
+  getChatService: () => getOrCreateService('chatService', createChatService),
+  getUserService: () => getOrCreateService('userService', createUserService),
+};
