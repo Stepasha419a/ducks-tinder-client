5dc1084279fa166b91c8c40b55f54af1df12c1e8
stepan <dark_magic419a@mail.ru>
2025-07-20 16:40:06 +0300

    feature: common-package create-user-service

--- packages/common/src/shared/api/services/user/user.service.ts
+++ packages/common/src/shared/api/services/user/user.service.ts
@@ -8,122 +8,125 @@ import type {
   PairsInfo,
   UserService,
 } from './user-service.interface';
-import { LIB_SETTINGS } from '@shared/lib';
+import { COMMON_LIB_SETTINGS } from '@shared/lib';
 
-export const userService: UserService = getMockableService(
-  {
-    async getMe() {
-      return instance.get<User>(`${LIB_SETTINGS.USER_SERVICE_URL}/user/me`);
+export const createUserService = (): UserService =>
+  getMockableService(
+    {
+      async getMe() {
+        return instance.get<User>(
+          `${COMMON_LIB_SETTINGS.USER_SERVICE_URL}/user/me`
+        );
+      },
+
+      async getMatchUsers(take: number, skipUserIds?: string[]) {
+        const config = getOptionalAbortControllerConfig('user/match', true);
+
+        return instance.get<ShortUser[]>(
+          `${COMMON_LIB_SETTINGS.USER_SERVICE_URL}/user/match`,
+          {
+            params: {
+              take,
+              skipUserIds,
+            },
+            ...config,
+          }
+        );
+      },
+
+      async updateUser(data: Partial<User>) {
+        return instance.patch<User>(
+          `${COMMON_LIB_SETTINGS.USER_SERVICE_URL}/user`,
+          data
+        );
+      },
+
+      async updateUserPlace(latitude: number, longitude: number) {
+        return instance.patch<User>(
+          `${COMMON_LIB_SETTINGS.USER_SERVICE_URL}/user/place`,
+          {
+            latitude,
+            longitude,
+          }
+        );
+      },
+
+      async savePicture(picture: Blob) {
+        return instance.post<User>(
+          `${COMMON_LIB_SETTINGS.USER_SERVICE_URL}/user/picture`,
+          { picture },
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+          }
+        );
+      },
+
+      async deletePicture(id: string) {
+        return instance.put<User>(
+          `${COMMON_LIB_SETTINGS.USER_SERVICE_URL}/user/picture/${id}`
+        );
+      },
+
+      async mixPictures(pictureOrders: number[]) {
+        return instance.put<User>(
+          `${COMMON_LIB_SETTINGS.USER_SERVICE_URL}/user/picture/mix`,
+          {
+            pictureOrders,
+          }
+        );
+      },
+
+      async getPairs(params: PairFilterParams, abortPrevious?: boolean) {
+        const config = getOptionalAbortControllerConfig(
+          'user/pairs',
+          abortPrevious
+        );
+
+        const res = await instance.get<ShortUser[]>(
+          `${COMMON_LIB_SETTINGS.USER_SERVICE_URL}/user/pairs`,
+          { params, ...config }
+        );
+
+        return res;
+      },
+
+      async getPairsInfo() {
+        return instance.get<PairsInfo>(
+          `${COMMON_LIB_SETTINGS.USER_SERVICE_URL}/user/pairs/info`
+        );
+      },
+
+      async acceptPair(pairId: string) {
+        return instance.post<ShortUser>(
+          `${COMMON_LIB_SETTINGS.USER_SERVICE_URL}/user/pairs/${pairId}`
+        );
+      },
+
+      async deletePair(pairId: string) {
+        return instance.put<ShortUser>(
+          `${COMMON_LIB_SETTINGS.USER_SERVICE_URL}/user/pairs/${pairId}`
+        );
+      },
+
+      async likeUser(userId: string) {
+        return instance.post<ShortUser>(
+          `${COMMON_LIB_SETTINGS.USER_SERVICE_URL}/user/like/${userId}`
+        );
+      },
+
+      async dislikeUser(userId: string) {
+        return instance.post<ShortUser>(
+          `${COMMON_LIB_SETTINGS.USER_SERVICE_URL}/user/dislike/${userId}`
+        );
+      },
+
+      async returnUser() {
+        return instance.put<ShortUser>(
+          `${COMMON_LIB_SETTINGS.USER_SERVICE_URL}/user/return`
+        );
+      },
     },
-
-    async getMatchUsers(take: number, skipUserIds?: string[]) {
-      const config = getOptionalAbortControllerConfig('user/match', true);
-
-      return instance.get<ShortUser[]>(
-        `${LIB_SETTINGS.USER_SERVICE_URL}/user/match`,
-        {
-          params: {
-            take,
-            skipUserIds,
-          },
-          ...config,
-        }
-      );
-    },
-
-    async updateUser(data: Partial<User>) {
-      return instance.patch<User>(
-        `${LIB_SETTINGS.USER_SERVICE_URL}/user`,
-        data
-      );
-    },
-
-    async updateUserPlace(latitude: number, longitude: number) {
-      return instance.patch<User>(
-        `${LIB_SETTINGS.USER_SERVICE_URL}/user/place`,
-        {
-          latitude,
-          longitude,
-        }
-      );
-    },
-
-    async savePicture(picture: Blob) {
-      return instance.post<User>(
-        `${LIB_SETTINGS.USER_SERVICE_URL}/user/picture`,
-        { picture },
-        {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        }
-      );
-    },
-
-    async deletePicture(id: string) {
-      return instance.put<User>(
-        `${LIB_SETTINGS.USER_SERVICE_URL}/user/picture/${id}`
-      );
-    },
-
-    async mixPictures(pictureOrders: number[]) {
-      return instance.put<User>(
-        `${LIB_SETTINGS.USER_SERVICE_URL}/user/picture/mix`,
-        {
-          pictureOrders,
-        }
-      );
-    },
-
-    async getPairs(params: PairFilterParams, abortPrevious?: boolean) {
-      const config = getOptionalAbortControllerConfig(
-        'user/pairs',
-        abortPrevious
-      );
-
-      const res = await instance.get<ShortUser[]>(
-        `${LIB_SETTINGS.USER_SERVICE_URL}/user/pairs`,
-        { params, ...config }
-      );
-
-      return res;
-    },
-
-    async getPairsInfo() {
-      return instance.get<PairsInfo>(
-        `${LIB_SETTINGS.USER_SERVICE_URL}/user/pairs/info`
-      );
-    },
-
-    async acceptPair(pairId: string) {
-      return instance.post<ShortUser>(
-        `${LIB_SETTINGS.USER_SERVICE_URL}/user/pairs/${pairId}`
-      );
-    },
-
-    async deletePair(pairId: string) {
-      return instance.put<ShortUser>(
-        `${LIB_SETTINGS.USER_SERVICE_URL}/user/pairs/${pairId}`
-      );
-    },
-
-    async likeUser(userId: string) {
-      return instance.post<ShortUser>(
-        `${LIB_SETTINGS.USER_SERVICE_URL}/user/like/${userId}`
-      );
-    },
-
-    async dislikeUser(userId: string) {
-      return instance.post<ShortUser>(
-        `${LIB_SETTINGS.USER_SERVICE_URL}/user/dislike/${userId}`
-      );
-    },
-
-    async returnUser() {
-      return instance.put<ShortUser>(
-        `${LIB_SETTINGS.USER_SERVICE_URL}/user/return`
-      );
-    },
-  },
-  userMockService
-);
+    userMockService
+  );
