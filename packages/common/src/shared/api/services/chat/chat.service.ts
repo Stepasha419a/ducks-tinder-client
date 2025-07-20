0623da2681915818ca7d32578337b48b0b5782e5
stepan <dark_magic419a@mail.ru>
2025-07-20 16:40:11 +0300

    feature: common-package create-chat-service

--- packages/common/src/shared/api/services/chat/chat.service.ts
+++ packages/common/src/shared/api/services/chat/chat.service.ts
@@ -1,10 +1,10 @@
 import type { Chat, ShortUser } from '@shared/api';
 import { instance } from '@shared/api';
-import { LIB_SETTINGS, type PaginationParams } from '@shared/lib';
+import { COMMON_LIB_SETTINGS, type PaginationParams } from '@shared/lib';
 
 import { getMockableService } from '../mock';
 import { chatMockService } from './chat.mock-service';
-import { chatSocket } from './chat.socket';
+import { createChatSocket } from './chat.socket';
 import type {
   ChatConnectReturn,
   ChatsConnectReturn,
@@ -13,68 +13,75 @@ import type {
   ShortMessagesPagination,
 } from './chat-service.interface';
 
-export const chatService: ChatService = getMockableService(
-  {
-    async getChat(chatId: string) {
-      return instance.get<Chat>(
-        `${LIB_SETTINGS.CHAT_SERVICE_URL}/chat/${chatId}`
-      );
+export const createChatService = (): ChatService => {
+  const chatSocket = createChatSocket();
+
+  return getMockableService(
+    {
+      async getChat(chatId: string) {
+        return instance.get<Chat>(
+          `${COMMON_LIB_SETTINGS.CHAT_SERVICE_URL}/chat/${chatId}`
+        );
+      },
+      async getChats(params: PaginationParams) {
+        return instance.get<Chat[]>(
+          `${COMMON_LIB_SETTINGS.CHAT_SERVICE_URL}/chat`,
+          {
+            params,
+          }
+        );
+      },
+      async getMessages(chatId: string, params: PaginationParams) {
+        return instance.get<ShortMessagesPagination>(
+          `${COMMON_LIB_SETTINGS.CHAT_SERVICE_URL}/chat/${chatId}/messages`,
+          { params }
+        );
+      },
+      async getNewMessagesCount() {
+        return instance.get<NewMessagesCount>(
+          `${COMMON_LIB_SETTINGS.CHAT_SERVICE_URL}/chat/new`
+        );
+      },
+      async getMember(memberId: string) {
+        return instance.get<ShortUser>(
+          `${COMMON_LIB_SETTINGS.CHAT_SERVICE_URL}/chat/member/${memberId}`
+        );
+      },
+      connect(): ChatsConnectReturn {
+        return chatSocket.connect();
+      },
+      connectChat(chatId: string): ChatConnectReturn | null {
+        return chatSocket.connectChat(chatId);
+      },
+      sendMessage(
+        chatId: string,
+        text: string,
+        repliedId: string | null = null
+      ): void {
+        chatSocket.sendMessage(chatId, text, repliedId);
+      },
+      deleteMessage(messageId: string): void {
+        chatSocket.deleteMessage(messageId);
+      },
+      editMessage(messageId: string, text: string): void {
+        chatSocket.editMessage(messageId, text);
+      },
+      blockChat(chatId: string): void {
+        chatSocket.blockChat(chatId);
+      },
+      unblockChat(chatId: string): void {
+        chatSocket.unblockChat(chatId);
+      },
+      deleteChat(chatId: string): void {
+        chatSocket.deleteChat(chatId);
+      },
+      disconnectChat(chatId: string): void {
+        chatSocket.disconnectChat(chatId);
+      },
+      disconnect(): void {
+        chatSocket.disconnect();
+      },
     },
-    async getChats(params: PaginationParams) {
-      return instance.get<Chat[]>(`${LIB_SETTINGS.CHAT_SERVICE_URL}/chat`, {
-        params,
-      });
-    },
-    async getMessages(chatId: string, params: PaginationParams) {
-      return instance.get<ShortMessagesPagination>(
-        `${LIB_SETTINGS.CHAT_SERVICE_URL}/chat/${chatId}/messages`,
-        { params }
-      );
-    },
-    async getNewMessagesCount() {
-      return instance.get<NewMessagesCount>(
-        `${LIB_SETTINGS.CHAT_SERVICE_URL}/chat/new`
-      );
-    },
-    async getMember(memberId: string) {
-      return instance.get<ShortUser>(
-        `${LIB_SETTINGS.CHAT_SERVICE_URL}/chat/member/${memberId}`
-      );
-    },
-    connect(): ChatsConnectReturn {
-      return chatSocket.connect();
-    },
-    connectChat(chatId: string): ChatConnectReturn | null {
-      return chatSocket.connectChat(chatId);
-    },
-    sendMessage(
-      chatId: string,
-      text: string,
-      repliedId: string | null = null
-    ): void {
-      chatSocket.sendMessage(chatId, text, repliedId);
-    },
-    deleteMessage(messageId: string): void {
-      chatSocket.deleteMessage(messageId);
-    },
-    editMessage(messageId: string, text: string): void {
-      chatSocket.editMessage(messageId, text);
-    },
-    blockChat(chatId: string): void {
-      chatSocket.blockChat(chatId);
-    },
-    unblockChat(chatId: string): void {
-      chatSocket.unblockChat(chatId);
-    },
-    deleteChat(chatId: string): void {
-      chatSocket.deleteChat(chatId);
-    },
-    disconnectChat(chatId: string): void {
-      chatSocket.disconnectChat(chatId);
-    },
-    disconnect(): void {
-      chatSocket.disconnect();
-    },
-  },
-  chatMockService
-);
+    chatMockService
+  );
+};
