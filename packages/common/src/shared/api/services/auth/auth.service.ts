b4e9f2636bb22fcb7c99c3b732a20c8d4a48740a
stepan <dark_magic419a@mail.ru>
2025-07-20 16:40:15 +0300

    feature: common-package create-auth-service

--- packages/common/src/shared/api/services/auth/auth.service.ts
+++ packages/common/src/shared/api/services/auth/auth.service.ts
@@ -3,34 +3,37 @@ import { instance } from '@shared/api';
 import { getMockableService } from '../mock';
 import type { AuthResponse, AuthService } from './auth.interfaces';
 import { authMockService } from './auth.mock-service';
-import { LIB_SETTINGS } from '@shared/lib';
+import { COMMON_LIB_SETTINGS } from '@shared/lib';
 
-export const authService: AuthService = getMockableService(
-  {
-    async refresh() {
-      return instance.get<AuthResponse>(
-        `${LIB_SETTINGS.AUTH_SERVICE_URL}/auth/refresh`
-      );
+export const createAuthService = (): AuthService =>
+  getMockableService(
+    {
+      async refresh() {
+        return instance.get<AuthResponse>(
+          `${COMMON_LIB_SETTINGS.AUTH_SERVICE_URL}/auth/refresh`
+        );
+      },
+      async registration(email: string, name: string, password: string) {
+        return instance.post<AuthResponse>(
+          `${COMMON_LIB_SETTINGS.AUTH_SERVICE_URL}/auth/register`,
+          {
+            email,
+            name,
+            password,
+          }
+        );
+      },
+      async login(email: string, password: string) {
+        return instance.post<AuthResponse>(
+          `${COMMON_LIB_SETTINGS.AUTH_SERVICE_URL}/auth/login`,
+          { email, password }
+        );
+      },
+      async logout() {
+        return instance.patch(
+          `${COMMON_LIB_SETTINGS.AUTH_SERVICE_URL}/auth/logout`
+        );
+      },
     },
-    async registration(email: string, name: string, password: string) {
-      return instance.post<AuthResponse>(
-        `${LIB_SETTINGS.AUTH_SERVICE_URL}/auth/register`,
-        {
-          email,
-          name,
-          password,
-        }
-      );
-    },
-    async login(email: string, password: string) {
-      return instance.post<AuthResponse>(
-        `${LIB_SETTINGS.AUTH_SERVICE_URL}/auth/login`,
-        { email, password }
-      );
-    },
-    async logout() {
-      return instance.patch(`${LIB_SETTINGS.AUTH_SERVICE_URL}/auth/logout`);
-    },
-  },
-  authMockService
-);
+    authMockService
+  );
