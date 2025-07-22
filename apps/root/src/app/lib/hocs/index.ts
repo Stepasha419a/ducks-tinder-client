3b948a15a0e73f1242d6a4cf19849187825b1554
stepan <dark_magic419a@mail.ru>
2025-07-22 23:01:59 +0300

    feature: root-app apply lib settings

--- apps/root/src/app/lib/hocs/index.ts
+++ apps/root/src/app/lib/hocs/index.ts
@@ -1,4 +1,5 @@
 import {
+  setCommonLibSettings,
   WithChatConnection,
   WithErrorFallback,
   WithNewMessagesCount,
@@ -9,9 +10,19 @@ import { WithCheckedFields } from '@features/WithCheckedFields';
 import { WithUserData } from '@features/WithUserData';
 import { WithInitialLoading, WithPairsInfo } from '@entities/user';
 import { authHocComposition, privateHocComposition } from '@shared/lib';
+import { setUiLibSettings } from '@ducks-tinder-client/ui';
 
 export { WithBrowserRouter } from './BrowserRouter';
 export { withAppHocs } from './withAppHocs';
+
+setUiLibSettings({ IMAGE_BASE_URL: window._env_.VITE_FILE_SERVICE_URL });
+setCommonLibSettings({
+  AUTH_SERVICE_URL: window._env_.VITE_AUTH_SERVICE_URL,
+  CHAT_SERVICE_URL: window._env_.VITE_CHAT_SERVICE_URL,
+  FILE_SERVICE_URL: window._env_.VITE_FILE_SERVICE_URL,
+  USER_SERVICE_URL: window._env_.VITE_USER_SERVICE_URL,
+  WITH_MOCKS: window._env_.VITE_MODE === 'demo',
+});
 
 privateHocComposition.addHocs([
   WithInitialLoading,
