1dae559f1b5090cb9c473eb145b5539ab67111b0
stepan <dark_magic419a@mail.ru>
2025-07-24 18:18:37 +0300

    feature: policy-app apply lib settings

--- apps/policy/src/app/App.tsx
+++ apps/policy/src/app/App.tsx
@@ -1,10 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import { ThemeProvider } from '@ducks-tinder-client/ui';
+import { ThemeProvider, setUiLibSettings } from '@ducks-tinder-client/ui';
 
 import PolicyPage from './policy';
 import '@ducks-tinder-client/ui/dist/esm/index.css';
+
+setUiLibSettings({ IMAGE_BASE_URL: window._env_.VAR_FILE_SERVICE_URL });
 
 const App = () => {
   return (
