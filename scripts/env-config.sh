27a2fc560e08499bb578d246d7cc49fd9121c3d1
stepan <dark_magic419a@mail.ru>
2025-07-25 20:43:40 +0300

    refactor: scripts env-config change config-file path

--- scripts/env-config.sh
+++ scripts/env-config.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-echo "window._env_ = {" > /usr/src/app/env-config.js
+echo "window._env_ = {" > /usr/share/nginx/html/env-config.js
 
 printenv | grep '^VAR_' | while IFS='=' read -r key value; do
   esc_value=$(printf '%s\n' "$value" | sed 's/"/\\"/g')
-  echo "  \"$key\": \"$esc_value\"," >> /usr/src/app/env-config.js
+  echo "  \"$key\": \"$esc_value\"," >> /usr/share/nginx/html/env-config.js
 done
 
-echo "}" >> /usr/src/app/env-config.js
+echo "}" >> /usr/share/nginx/html/env-config.js
