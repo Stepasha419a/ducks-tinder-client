5a5d894ed37cc97be6ebf3688cf968c917118494
stepan <dark_magic419a@mail.ru>
2025-07-25 20:42:45 +0300

    refactor: scripts entrypoint change env name

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,6 +2,6 @@
 
 /usr/src/app/env-config.sh
 
-envsubst '${PROXY_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${VAR_PROXY_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec nginx -g 'daemon off;'
