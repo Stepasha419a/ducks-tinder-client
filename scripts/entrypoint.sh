8a1e19509997bda8c0b08a909f00459dae3f9941
stepan <dark_magic419a@mail.ru>
2025-08-06 22:34:45 +0300

    feature: scripts entrypoint add vars validation

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,6 +2,17 @@
 
 /usr/src/app/env-config.sh
 
-envsubst '${VAR_PROXY_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+REQUIRED_VARS="PROXY_SERVER_NAME"
+
+for var in $REQUIRED_VARS; do
+  if [ -z "${!var}" ]; then
+    echo "Error: var $var is unset"
+    exit 1
+  fi
+done
+
+SUBST_LIST=$(printf '${%s} ' $REQUIRED_VARS)
+
+envsubst "$SUBST_LIST" < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec nginx -g 'daemon off;'
