2be95106173cc26a6ee247390c3b3251f53620ce
stepan <dark_magic419a@mail.ru>
2025-08-30 10:08:08 +0300

    feature: root-app webpack-production use promise for policy-app url

--- apps/root/config/production.config.js
+++ apps/root/config/production.config.js
@@ -5,7 +5,28 @@ export default (env) =>
     name: 'rootApp',
     envPath: env.envPath,
     remotes: {
-      policyApp: 'policyApp@https://localhost/remote/policy/remoteEntry.js',
+      policyApp: `promise new Promise(resolve => {
+        const rootPath = window._env_.VAR_ROOT_PATH ?? ''
+        const optionalTrailingSlash = rootPath.endsWith('/') ? '' : '/'
+        const rootPathTrailingSlash = rootPath + optionalTrailingSlash
+        const url = rootPathTrailingSlash + 'remote/policy/remoteEntry.js';
+        const script = document.createElement('script');
+        script.src = url;
+        script.onload = () => {
+          const proxy = {
+            get: (request) => window.policyApp.get(request),
+            init: (arg) => {
+              try {
+                return window.policyApp.init(arg);
+              } catch (e) {
+                console.error("policyApp already initialized");
+              }
+            }
+          };
+          resolve(proxy);
+        };
+        document.head.appendChild(script);
+      })`,
       chatApp: 'chatApp@https://localhost/remote/chat/remoteEntry.js',
     },
     packagePath: './package.json',
