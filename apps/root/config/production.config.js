c243b4db71d94ffb8e065350de4c0e6f5038b790
stepan <dark_magic419a@mail.ru>
2025-08-30 10:08:14 +0300

    feature: root-app webpack-production use promise for chat-app url

--- apps/root/config/production.config.js
+++ apps/root/config/production.config.js
@@ -27,7 +27,28 @@ export default (env) =>
         };
         document.head.appendChild(script);
       })`,
-      chatApp: 'chatApp@https://localhost/remote/chat/remoteEntry.js',
+      chatApp: `promise new Promise(resolve => {
+        const rootPath = window._env_.VAR_ROOT_PATH ?? ''
+        const optionalTrailingSlash = rootPath.endsWith('/') ? '' : '/'
+        const rootPathTrailingSlash = rootPath + optionalTrailingSlash
+        const url = rootPathTrailingSlash + 'remote/chat/remoteEntry.js';
+        const script = document.createElement('script');
+        script.src = url;
+        script.onload = () => {
+          const proxy = {
+            get: (request) => window.chatApp.get(request),
+            init: (arg) => {
+              try {
+                return window.chatApp.init(arg);
+              } catch (e) {
+                console.error("chatApp already initialized");
+              }
+            }
+          };
+          resolve(proxy);
+        };
+        document.head.appendChild(script);
+      })`,
     },
     packagePath: './package.json',
     mediaPublicPath: '../../public',
