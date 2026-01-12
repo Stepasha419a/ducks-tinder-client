ee9e0d185f9ddeffdf08f3c91cbe7c57d047d8b9
Stepasha419a <Dark_Magic419a@mail.ru>
2026-01-12 17:21:23 +0700

    feature: common-package specify root-url and socket-path setting vars

--- packages/common/src/shared/lib/constants/commonLibSettings.ts
+++ packages/common/src/shared/lib/constants/commonLibSettings.ts
@@ -1,14 +1,18 @@
 export interface CommonLibSettings {
+  CLIENT_ROOT_URL: string;
   AUTH_SERVICE_URL: string;
   CHAT_SERVICE_URL: string;
+  CHAT_SERVICE_SOCKET_PATH: string;
   FILE_SERVICE_URL: string;
   USER_SERVICE_URL: string;
   WITH_MOCKS: boolean;
 }
 
 export const COMMON_LIB_SETTINGS: CommonLibSettings = {
+  CLIENT_ROOT_URL: '',
   AUTH_SERVICE_URL: '',
   CHAT_SERVICE_URL: '',
+  CHAT_SERVICE_SOCKET_PATH: '',
   FILE_SERVICE_URL: '',
   USER_SERVICE_URL: '',
   WITH_MOCKS: false,
