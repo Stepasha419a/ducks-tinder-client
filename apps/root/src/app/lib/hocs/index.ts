f7dae9582a79ccc9f8f29f27ed06610604c591ec
Stepasha419a <Dark_Magic419a@mail.ru>
2026-01-18 21:40:29 +0700

    feature: root-app provide new common-lib vars

--- apps/root/src/app/lib/hocs/index.ts
+++ apps/root/src/app/lib/hocs/index.ts
@@ -17,8 +17,10 @@ export { withAppHocs } from './withAppHocs';
 
 setUiLibSettings({ IMAGE_BASE_URL: window._env_.VAR_FILE_SERVICE_URL });
 setCommonLibSettings({
+  CLIENT_ROOT_URL: window._env_.VAR_CLIENT_ROOT_URL,
   AUTH_SERVICE_URL: window._env_.VAR_AUTH_SERVICE_URL,
   CHAT_SERVICE_URL: window._env_.VAR_CHAT_SERVICE_URL,
+  CHAT_SERVICE_SOCKET_PATH: window._env_.VAR_CHAT_SERVICE_SOCKET_PATH,
   FILE_SERVICE_URL: window._env_.VAR_FILE_SERVICE_URL,
   USER_SERVICE_URL: window._env_.VAR_USER_SERVICE_URL,
   WITH_MOCKS: window._env_.VAR_MODE === 'demo',
