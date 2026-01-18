88fa1d3b64b34e8a2287174e7a64c6622588cf1c
Stepasha419a <Dark_Magic419a@mail.ru>
2026-01-18 21:39:47 +0700

    feature: chat-app provide new common-lib vars

--- apps/chat/src/app/App.tsx
+++ apps/chat/src/app/App.tsx
@@ -24,8 +24,10 @@ import * as styles from './App.module.scss';
 
 setUiLibSettings({ IMAGE_BASE_URL: window._env_.VAR_FILE_SERVICE_URL });
 setCommonLibSettings({
+  CLIENT_ROOT_URL: window._env_.VAR_CLIENT_ROOT_URL,
   AUTH_SERVICE_URL: window._env_.VAR_AUTH_SERVICE_URL,
   CHAT_SERVICE_URL: window._env_.VAR_CHAT_SERVICE_URL,
+  CHAT_SERVICE_SOCKET_PATH: window._env_.VAR_CHAT_SERVICE_SOCKET_PATH,
   FILE_SERVICE_URL: window._env_.VAR_FILE_SERVICE_URL,
   USER_SERVICE_URL: window._env_.VAR_USER_SERVICE_URL,
   WITH_MOCKS: window._env_.VAR_MODE === 'demo',
