2d43c51728c8683e1d45d1530d6b6b565a1de50e
Stepasha419a <Dark_Magic419a@mail.ru>
2026-01-18 21:36:06 +0700

    feature: root-app add new env var types

--- apps/root/src/app/lib/types/env.d.ts
+++ apps/root/src/app/lib/types/env.d.ts
@@ -3,8 +3,10 @@ export {};
 declare global {
   interface Window {
     _env_: {
+      VAR_CLIENT_ROOT_URL: string;
       VAR_USER_SERVICE_URL: string;
       VAR_CHAT_SERVICE_URL: string;
+      VAR_CHAT_SERVICE_SOCKET_PATH: string;
       VAR_FILE_SERVICE_URL: string;
       VAR_AUTH_SERVICE_URL: string;
       VAR_MODE: string;
