9f58c3407ee58a080f2e6c883f4ced53755543cd
Stepasha419a <Dark_Magic419a@mail.ru>
2026-01-18 21:36:22 +0700

    feature: policy-app add new env var types and matomo

--- apps/policy/src/app/lib/types/env.d.ts
+++ apps/policy/src/app/lib/types/env.d.ts
@@ -3,12 +3,16 @@ export {};
 declare global {
   interface Window {
     _env_: {
+      VAR_CLIENT_ROOT_URL: string;
       VAR_USER_SERVICE_URL: string;
       VAR_CHAT_SERVICE_URL: string;
+      VAR_CHAT_SERVICE_SOCKET_PATH: string;
       VAR_FILE_SERVICE_URL: string;
       VAR_AUTH_SERVICE_URL: string;
       VAR_MODE: string;
       VAR_ROOT_PATH?: string;
+      VAR_MATOMO_URL?: string;
+      VAR_MATOMO_SITE_ID?: string;
     };
   }
 }
