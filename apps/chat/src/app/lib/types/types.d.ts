028ae474dee02e80032f54e7efc0bfef9a951110
stepan <dark_magic419a@mail.ru>
2026-03-11 23:56:03 +0300

    feature: chat-app chat-store types

--- apps/chat/src/app/lib/types/types.d.ts
+++ apps/chat/src/app/lib/types/types.d.ts
@@ -1,7 +1,12 @@
+import type { chatStore } from '@app/model/store';
 import type { store } from '@ducks-tinder-client/common';
 
 declare global {
   export type RootStore = ReturnType<typeof store>;
   export type RootState = ReturnType<typeof store.getState>;
   export type AppDispatch = typeof store.dispatch;
+
+  export type ChatStore = ReturnType<typeof chatStore>;
+  export type ChatState = ReturnType<typeof chatStore.getState>;
+  export type ChatDispatch = typeof chatStore.dispatch;
 }
