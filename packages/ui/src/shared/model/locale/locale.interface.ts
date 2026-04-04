195ff0e39371a08a6ea32680649347373c2ae327
stepan <dark_magic419a@mail.ru>
2026-04-04 17:03:15 +0300

    feature: ui-package locale interface

---
+++ packages/ui/src/shared/model/locale/locale.interface.ts
@@ -0,0 +1,13 @@
+export interface Locale {
+  livesIn: string;
+  distance: string;
+  altAvatar: string;
+  ariaOpenFullPreview: string;
+  ariaShowAllInterests: string;
+  unknownPlace: string;
+  moreAboutMe: string;
+  lifestyle: string;
+  interests: string;
+  close: string;
+  showAll: string;
+}
