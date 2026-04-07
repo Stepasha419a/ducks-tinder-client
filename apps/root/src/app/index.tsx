dde87737eddf97e67de1e3c6fd01e6a6a4b45f0b
stepan <dark_magic419a@mail.ru>
2026-04-07 21:01:19 +0300

    feature: root-app init i18n

--- apps/root/src/app/index.tsx
+++ apps/root/src/app/index.tsx
@@ -6,6 +6,20 @@ import { withAppHocs, OptionalMetricsProvider } from './lib';
 import { Routing } from './routing';
 import '@ducks-tinder-client/ui/dist/esm/index.css';
 
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: false,
+    interpolation: {
+      escapeValue: false,
+    },
+    load: 'languageOnly',
+    ns: ['common'],
+    defaultNS: 'common',
+  });
 // TODO: decompose ThemeProvider into hoc
 function App(): ReactElement {
   return (
