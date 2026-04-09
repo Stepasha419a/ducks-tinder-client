22738ef47405b5c286391b458eb42e625029640e
stepan <dark_magic419a@mail.ru>
2026-04-09 07:14:42 +0300

    feature: root-app place-setting i18n

--- apps/root/src/widgets/PlaceSetting/ui/PlaceSetting.tsx
+++ apps/root/src/widgets/PlaceSetting/ui/PlaceSetting.tsx
@@ -6,8 +6,11 @@ import { PlacesGeolocation } from '@features/PlacesGeolocation';
 import { Map } from '@entities/user';
 
 import * as styles from './PlaceSetting.module.scss';
+import { useTranslation } from 'react-i18next';
 
 export const PlaceSetting = () => {
+  const { t } = useTranslation();
+
   const isMobile = useAdaptiveMediaQuery('(max-width: 900px)');
 
   const url = isMobile ? ROUTES.SETTINGS : ROUTES.PROFILE;
@@ -15,9 +18,9 @@ export const PlaceSetting = () => {
   return (
     <div className={styles.setting}>
       <div className={styles.head}>
-        <div className={styles.title}>Place</div>
+        <div className={styles.title}>{t('profile.settings.place.title')}</div>
         <Link to={url} className={styles.submit}>
-          Submit
+          {t('submit')}
         </Link>
       </div>
       <Map />
