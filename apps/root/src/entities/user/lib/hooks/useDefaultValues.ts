ddca4f7c144f77e35ca11de4d910629c188b2c70
stepan <dark_magic419a@mail.ru>
2026-04-11 07:51:35 +0300

    feature: root-app use-default-values return language

--- apps/root/src/entities/user/lib/hooks/useDefaultValues.ts
+++ apps/root/src/entities/user/lib/hooks/useDefaultValues.ts
@@ -2,15 +2,26 @@ import type { Place, Setting } from '@ducks-tinder-client/common';
 import { useAppSelector } from '@ducks-tinder-client/common';
 
 import { SettingNameEnum } from '../constants';
+import { useTranslation } from 'react-i18next';
+import { SupportedLanguage } from '@shared/lib';
 
 export function useDefaultValues(
-  settingName: Setting
+  settingName: Setting | null
 ): string | number | boolean | string[] | Place | null {
+  const { i18n } = useTranslation();
+  const language = i18n.resolvedLanguage || SupportedLanguage.English;
   const currentUser = useAppSelector((state) => state.user.currentUser!);
   const email = useAppSelector((state) => state.auth.authData!.email);
 
+  if (!settingName) {
+    return null;
+  }
+
   if (settingName === SettingNameEnum.EMAIL) {
     return email;
+  }
+  if (settingName === SettingNameEnum.LANGUAGE) {
+    return language;
   }
 
   return currentUser[settingName];
