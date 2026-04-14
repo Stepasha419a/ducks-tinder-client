f3c62b7a1324b4c3a217798cd29c63910a694f05
stepan <dark_magic419a@mail.ru>
2026-04-14 08:39:15 +0300

    feature: root-app setting-validation i18n

--- apps/root/src/features/SettingsBlock/lib/hooks/useCurrentValidation.ts
+++ apps/root/src/features/SettingsBlock/lib/hooks/useCurrentValidation.ts
@@ -3,6 +3,7 @@ import type { RegisterOptions } from 'react-hook-form';
 import { EMAIL_REGEXP } from '@ducks-tinder-client/common';
 
 import { useSettingUrl } from '@entities/user';
+import { useTranslation } from 'react-i18next';
 
 export interface SettingFieldValues {
   input: string;
@@ -11,32 +12,34 @@ export interface SettingFieldValues {
 export function useCurrentValidation():
   | RegisterOptions<SettingFieldValues, 'input'>
   | undefined {
+  const { t } = useTranslation();
+
   const setting = useSettingUrl();
 
   if (!setting) {
     return undefined;
   }
 
-  const { formName, validation } = setting;
+  const { validation } = setting;
 
   return {
-    required: `${formName} is required`,
+    required: t('validation.required'),
     minLength: validation?.min
       ? {
           value: validation.min,
-          message: `${formName} must be more than ${validation.min}`,
+          message: t('validation.lengthMin', { count: validation.min }),
         }
       : undefined,
     maxLength: validation?.max
       ? {
           value: validation.max,
-          message: `${formName} must be less than ${validation.max}`,
+          message: t('validation.lengthMax', { count: validation.max }),
         }
       : undefined,
     pattern: validation?.email
       ? {
           value: EMAIL_REGEXP,
-          message: 'Incorrect email',
+          message: t('validation.email'),
         }
       : undefined,
   };
