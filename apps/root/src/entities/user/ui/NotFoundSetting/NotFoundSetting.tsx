4fba4a9708a74e1f6579c5b2feeed1d22e6f745e
stepan <dark_magic419a@mail.ru>
2026-04-23 08:13:48 +0300

    refactor: root-app not-found-setting i18n

--- apps/root/src/entities/user/ui/NotFoundSetting/NotFoundSetting.tsx
+++ apps/root/src/entities/user/ui/NotFoundSetting/NotFoundSetting.tsx
@@ -4,18 +4,21 @@ import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import * as styles from './NotFoundSetting.module.scss';
+import { useTranslation } from 'react-i18next';
 
 interface NotFoundSettingProps {
   url: string;
 }
 
 export const NotFoundSetting: FC<NotFoundSettingProps> = ({ url }) => {
+  const { t } = useTranslation();
+
   return (
     <div>
       <div className={styles.notFound}>
         <Link to={url} className={styles.link}>
           <FontAwesomeIcon icon={faArrowLeft} className={styles.icon} />
-          <span className={styles.text}>Such setting was not found</span>
+          <span className={styles.text}>{t('profile.settings.notFound')}</span>
         </Link>
       </div>
     </div>
