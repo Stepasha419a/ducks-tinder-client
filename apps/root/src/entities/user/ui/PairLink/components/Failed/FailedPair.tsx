0d6dc9a2ee63dda295ca3d74dc523040ab590e1d
stepan <dark_magic419a@mail.ru>
2026-04-23 17:34:33 +0300

    refactor: root-app failed-pair i18n

--- apps/root/src/entities/user/ui/PairLink/components/Failed/FailedPair.tsx
+++ apps/root/src/entities/user/ui/PairLink/components/Failed/FailedPair.tsx
@@ -3,12 +3,15 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import * as styles from './FailedPair.module.scss';
+import { useTranslation } from 'react-i18next';
 
 export const FailedPair: FC = () => {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.noPairs}>
       <FontAwesomeIcon icon={faHeart} className={styles.icon} />
-      <div>You don't have likes. Like someone to have a like too</div>
+      <div>{t('pairs.list.noLikes')}</div>
     </div>
   );
 };
