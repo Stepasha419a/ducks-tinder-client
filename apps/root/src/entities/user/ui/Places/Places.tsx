b7b0eb036f338d79324ffaf484d301e10fef007e
stepan <dark_magic419a@mail.ru>
2026-04-23 08:13:34 +0300

    refactor: root-app places-setting i18n

--- apps/root/src/entities/user/ui/Places/Places.tsx
+++ apps/root/src/entities/user/ui/Places/Places.tsx
@@ -8,25 +8,30 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useAppSelector } from '@ducks-tinder-client/common';
 
 import * as styles from './Places.module.scss';
+import { useTranslation } from 'react-i18next';
 
 interface PlacesProps {
   refreshFeature: ReactElement;
 }
 
 export const Places: FC<PlacesProps> = ({ refreshFeature }) => {
+  const { t } = useTranslation();
+
   const place = useAppSelector((state) => state.user.currentUser!.place);
 
   return (
     <div className={styles.places}>
       <div className={styles.subhead}>
-        <div className={styles.subtitle}>Current positions</div>
+        <div className={styles.subtitle}>
+          {t('profile.settings.place.currentPositions')}
+        </div>
         {refreshFeature}
       </div>
       {place && (
         <div className={styles.item}>
           <FontAwesomeIcon className={styles.icon} icon={faLocationDot} />
           <div className={styles.position}>
-            <div>My current position</div>
+            <div>{t('profile.settings.place.myCurrentPosition')}</div>
             <div className={styles.name}>
               {place.name} {place.address}
             </div>
