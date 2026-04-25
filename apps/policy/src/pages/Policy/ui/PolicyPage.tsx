99fcdf93d0e44ca2de40ad6397de59e2a0f28245
stepan <dark_magic419a@mail.ru>
2026-04-25 09:01:34 +0300

    feature: policy-app page i18n

--- apps/policy/src/pages/Policy/ui/PolicyPage.tsx
+++ apps/policy/src/pages/Policy/ui/PolicyPage.tsx
@@ -4,34 +4,26 @@ import { Link } from 'react-router-dom';
 import { authDuck } from '@ducks-tinder-client/ui';
 
 import * as styles from './PolicyPage.module.scss';
+import { useTranslation } from 'react-i18next';
 
 const PolicyPage: FC = (): ReactElement => {
+  const { t } = useTranslation('policy');
+
   return (
     <div className={styles.policy}>
       <header className={styles.header}>
         <div className={styles.container}>
           <Link to="/" className={styles.link}>
             <img src={authDuck} alt="IMG" className={styles.image} />
-            <span className={styles.text}>Ducks Date</span>
+            <span className={styles.text}>{t('page_title')}</span>
           </Link>
         </div>
       </header>
       <hr className={styles.hr} />
       <div className={styles.content}>
         <div className={styles.container}>
-          <h1 className={styles.title}>Community Rules</h1>
-          <p className={styles.text}>
-            Welcome to the Ducks Date community. If you are honest, kind and
-            respectful towards others, we will always be glad to see you here.
-            However, if you behave differently, unfortunately, we will be forced
-            to part with you. Our goal is for users to be able to express
-            themselves freely as long as it doesn't hurt the feelings of others.
-            There is a single standard of behavior for everyone on Tinder. We
-            ask you to treat others with respect, think carefully about your
-            actions and follow the rules of our community both online and
-            offline. That's right: your offline behavior may lead to the
-            blocking of your Tinder account.
-          </p>
+          <h1 className={styles.title}>{t('paragraph_title')}</h1>
+          <p className={styles.text}>{t('paragraph_text')}</p>
         </div>
       </div>
     </div>
