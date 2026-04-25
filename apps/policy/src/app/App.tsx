14f1c84a84cb776e97f9fd7fa69317456408c051
stepan <dark_magic419a@mail.ru>
2026-04-25 09:02:16 +0300

    feature: policy-app init i18n

--- apps/policy/src/app/App.tsx
+++ apps/policy/src/app/App.tsx
@@ -6,7 +6,26 @@ import { ThemeProvider, setUiLibSettings } from '@ducks-tinder-client/ui';
 import PolicyPage from './policy';
 import '@ducks-tinder-client/ui/dist/esm/index.css';
 
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
+
 setUiLibSettings({ IMAGE_BASE_URL: window._env_.VAR_FILE_SERVICE_URL });
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: false,
+    interpolation: {
+      escapeValue: false,
+    },
+    ns: ['policy'],
+    defaultNS: 'policy',
+  });
 
 const App = () => {
   return (
