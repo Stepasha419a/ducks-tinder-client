0ad9805a9bbdda4ef188fc8d0c1c3e3bc524736b
stepan <dark_magic419a@mail.ru>
2026-04-25 09:11:15 +0300

    feature: chat-app choose-chat i18n

--- apps/chat/src/pages/IndexChatPage/ui/components/Choose/Choose.tsx
+++ apps/chat/src/pages/IndexChatPage/ui/components/Choose/Choose.tsx
@@ -2,13 +2,16 @@ import { faComment } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import * as styles from './Choose.module.scss';
+import { useTranslation } from 'react-i18next';
 
 export const Choose = () => {
+  const { t } = useTranslation('chat');
+
   return (
     <div className={styles.choose}>
       <div className={styles.inner}>
         <FontAwesomeIcon icon={faComment} className={styles.icon} />
-        <div className={styles.text}>Choose the chat</div>
+        <div className={styles.text}>{t('chooseChat')}</div>
       </div>
     </div>
   );
