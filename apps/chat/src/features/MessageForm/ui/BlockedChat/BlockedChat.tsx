5857b60e4e704005b5899565b2831d3c2cc0d3f1
stepan <dark_magic419a@mail.ru>
2026-04-26 08:56:02 +0300

    feature: chat-app blocked-chat i18n

--- apps/chat/src/features/MessageForm/ui/BlockedChat/BlockedChat.tsx
+++ apps/chat/src/features/MessageForm/ui/BlockedChat/BlockedChat.tsx
@@ -3,19 +3,22 @@ import { faLock } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import * as styles from './BlockedChat.module.scss';
+import { useTranslation } from 'react-i18next';
 
 interface BlockedChatProps {
   blockedByName: string;
 }
 
 export const BlockedChat: FC<BlockedChatProps> = ({ blockedByName }) => {
+  const { t } = useTranslation('chat');
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.blocked}>
         <FontAwesomeIcon icon={faLock} className={styles.icon} />
         <div className={styles.descr}>
           <span className={styles.name}>{blockedByName}</span>
-          <span className={styles.text}>blocked this chat</span>
+          <span className={styles.text}>{t('blockedThisChat')}</span>
         </div>
       </div>
     </div>
