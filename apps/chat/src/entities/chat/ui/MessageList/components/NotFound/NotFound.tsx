a43fa3613ace49f304dfeb35e6597a90c488056b
stepan <dark_magic419a@mail.ru>
2026-04-27 08:13:09 +0300

    feature: chat-app not-found-chat i18n

--- apps/chat/src/entities/chat/ui/MessageList/components/NotFound/NotFound.tsx
+++ apps/chat/src/entities/chat/ui/MessageList/components/NotFound/NotFound.tsx
@@ -3,13 +3,16 @@ import { faCommentSlash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import * as styles from './NotFound.module.scss';
+import { useTranslation } from 'react-i18next';
 
 export const NotFound = (): ReactElement => {
+  const { t } = useTranslation('chat');
+
   return (
     <div className={styles.noChats}>
       <div className={styles.inner}>
         <FontAwesomeIcon icon={faCommentSlash} className={styles.icon} />
-        <div className={styles.text}>Such chat was not found</div>
+        <div className={styles.text}>{t('chatNotFound')}</div>
       </div>
     </div>
   );
