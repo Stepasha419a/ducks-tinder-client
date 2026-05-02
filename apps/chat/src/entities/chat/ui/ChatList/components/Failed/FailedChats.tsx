1331d7d017cf5f30fdc8a7fae9d8e6d24a1d8e12
stepan <dark_magic419a@mail.ru>
2026-05-02 09:15:09 +0300

    feature: chat-app failed-chats i18n

--- apps/chat/src/entities/chat/ui/ChatList/components/Failed/FailedChats.tsx
+++ apps/chat/src/entities/chat/ui/ChatList/components/Failed/FailedChats.tsx
@@ -6,16 +6,17 @@ import classNames from 'classnames';
 import { useAdaptiveMediaQuery } from '@ducks-tinder-client/common';
 
 import * as styles from './FailedChats.module.scss';
+import { useTranslation } from 'react-i18next';
 
 export const FailedChats = (): ReactElement => {
+  const { t } = useTranslation('chat');
+
   const isMobile = useAdaptiveMediaQuery('(max-width: 900px)');
 
   return (
     <div className={classNames(styles.noPairs, isMobile && styles.mobile)}>
       <FontAwesomeIcon icon={faHeart} className={styles.icon} />
-      <div>
-        You don't have chats. Accept some pairs to have personal chats with them
-      </div>
+      <div>{t('noChats')}</div>
     </div>
   );
 };
