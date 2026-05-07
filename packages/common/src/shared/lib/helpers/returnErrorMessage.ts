90a5b3e821e6760c3764cc94a9966e7bd1f12466
stepan <dark_magic419a@mail.ru>
2026-05-07 21:47:37 +0300

    feature: common-package return-axios-error

--- packages/common/src/shared/lib/helpers/returnErrorMessage.ts
+++ packages/common/src/shared/lib/helpers/returnErrorMessage.ts
@@ -1,7 +1,18 @@
-import axios from 'axios';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { isAxiosError } from 'axios';
 
 export function returnErrorMessage(error: unknown): string {
-  if (error instanceof Error || axios.isAxiosError(error)) {
+  if (
+    isAxiosError(error) &&
+    error.response &&
+    typeof error.response.data === 'object' &&
+    error.response.data !== null &&
+    'message' in error.response.data &&
+    typeof error.response.data.message === 'string'
+  ) {
+    return error.response.data.message;
+  }
     return error.message;
   }
   if (typeof error === 'string') {
