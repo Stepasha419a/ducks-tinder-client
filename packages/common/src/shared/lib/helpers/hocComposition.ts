0165edd5620206f6fc5912fdc6da2c6c21d3a1ec
stepan <dark_magic419a@mail.ru>
2026-05-11 16:47:27 +0300

    feature: common-package hoc-composition set stages

--- packages/common/src/shared/lib/helpers/hocComposition.ts
+++ packages/common/src/shared/lib/helpers/hocComposition.ts
@@ -17,23 +17,45 @@ interface HocStage {
 }
 
 export class HocComposition {
-  hocs: HocFunc[];
+  stages = new Map<HocCompositionStage, HocFunc[]>();
   subscribers: Array<() => void> = [];
 
-  constructor(hocs: HocFunc[] = []) {
-    this.hocs = hocs;
-  }
+  constructor(stages: HocStage[] = []) {
+    this.stages.set(HocCompositionStage.BOOTSTRAPPING, []);
+    this.stages.set(HocCompositionStage.AUTH_CHECK, []);
+    this.stages.set(HocCompositionStage.USER_HYDRATION, []);
+    this.stages.set(HocCompositionStage.DATA_SYNCING, []);
+    this.stages.set(HocCompositionStage.COMPLETE, []);
 
-  addHocs(hocs: HocFunc[]) {
-    this.hocs = this.hocs.concat(hocs);
-
-    this.subscribers.forEach((subscriber) => {
-      subscriber();
+    stages.forEach((stage) => {
+      this.addHocs(stage.stage, stage.hocs, true);
     });
   }
 
+  addHocs(
+    stage: HocCompositionStage,
+    hocs: HocFunc[],
+    skipSubscribers?: boolean
+  ) {
+    this.stages.set(stage, [...(this.stages.get(stage) ?? []), ...hocs]);
+
+    if (skipSubscribers) {
+      this.subscribers.forEach((subscriber) => {
+        subscriber();
+      });
+    }
+  }
+
   appendHocs<T extends ComponentType>(component: T): T {
-    return compose(...this.hocs)(component) as T;
+    const hocs = [
+      ...(this.stages.get(HocCompositionStage.COMPLETE) ?? []),
+      ...(this.stages.get(HocCompositionStage.DATA_SYNCING) ?? []),
+      ...(this.stages.get(HocCompositionStage.USER_HYDRATION) ?? []),
+      ...(this.stages.get(HocCompositionStage.AUTH_CHECK) ?? []),
+      ...(this.stages.get(HocCompositionStage.BOOTSTRAPPING) ?? []),
+    ];
+
+    return compose(...hocs)(component) as T;
   }
 
   subscribe(callback: () => void) {
