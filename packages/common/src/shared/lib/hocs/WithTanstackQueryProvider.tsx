1eb30469a11d47477408458a8e537c09f43d82a5
stepan <dark_magic419a@mail.ru>
2026-05-17 15:08:50 +0300

    feature: common-app with-tanstack-query-provider

---
+++ packages/common/src/shared/lib/hocs/WithTanstackQueryProvider.tsx
@@ -0,0 +1,24 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import type { ComponentType } from 'react';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnReconnect: false,
+    },
+  },
+});
+
+export function WithTanstackQueryProvider<P extends object>(
+  Component: ComponentType<P>
+) {
+  const Wrapper = (props: P) => {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <Component {...props} />
+      </QueryClientProvider>
+    );
+  };
+
+  return Wrapper;
+}
