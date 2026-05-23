d54856c23a129eee4f2124b072c38c10c63a530f
stepan <dark_magic419a@mail.ru>
2026-05-23 16:15:09 +0300

    feature: auth-package auth-service

---
+++ packages/auth/src/shared/api/services/auth/auth.service.ts
@@ -0,0 +1,37 @@
+import { getMockableService } from '../mock';
+import type { AuthResponse, AuthService } from './auth.interfaces';
+import { authMockService } from './auth.mock-service';
+import { COMMON_LIB_SETTINGS, instance } from '@ducks-tinder-client/common';
+
+export const createAuthService = (): AuthService =>
+  getMockableService(
+    {
+      async refresh() {
+        return instance.get<AuthResponse>(
+          `${COMMON_LIB_SETTINGS.AUTH_SERVICE_URL}/auth/refresh`
+        );
+      },
+      async registration(email: string, name: string, password: string) {
+        return instance.post<AuthResponse>(
+          `${COMMON_LIB_SETTINGS.AUTH_SERVICE_URL}/auth/register`,
+          {
+            email,
+            name,
+            password,
+          }
+        );
+      },
+      async login(email: string, password: string) {
+        return instance.post<AuthResponse>(
+          `${COMMON_LIB_SETTINGS.AUTH_SERVICE_URL}/auth/login`,
+          { email, password }
+        );
+      },
+      async logout() {
+        return instance.patch(
+          `${COMMON_LIB_SETTINGS.AUTH_SERVICE_URL}/auth/logout`
+        );
+      },
+    },
+    authMockService
+  );
