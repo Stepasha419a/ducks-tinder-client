7890deb5d6c769fcf0ac9db37a4782db0d41d349
stepan <dark_magic419a@mail.ru>
2026-05-23 16:15:37 +0300

    feature: auth-package interceptors

---
+++ packages/auth/src/shared/api/interceptors.ts
@@ -0,0 +1,48 @@
+import type {
+  AxiosError,
+  AxiosInstance,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from 'axios';
+import axios from 'axios';
+
+import type { AuthResponse } from '@shared/api';
+import { COMMON_LIB_SETTINGS } from '@ducks-tinder-client/common';
+
+interface AxiosEditedConfig extends AxiosResponse {
+  _isRetry?: boolean;
+}
+export function setUpInterceptors(instance: AxiosInstance) {
+  instance.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+    config.headers.Authorization =
+      'Bearer ' + localStorage.getItem('accessToken')!;
+    return config;
+  });
+
+  instance.interceptors.response.use(
+    (config: AxiosEditedConfig) => config,
+    async (error: AxiosError<{ message: string; status: string }>) => {
+      const originalRequest = error.config as AxiosEditedConfig;
+      if (error.config?.url?.endsWith('/auth/refresh')) {
+        throw error;
+      }
+
+      if (
+        error.response?.status === 401 &&
+        !(error.config as AxiosEditedConfig)._isRetry
+      ) {
+        originalRequest._isRetry = true;
+        const response = await axios.get<AuthResponse>(
+          `${COMMON_LIB_SETTINGS.AUTH_SERVICE_URL}/auth/refresh`,
+          {
+            withCredentials: true,
+          }
+        );
+        localStorage.setItem('accessToken', response.data.accessToken);
+
+        return instance.request(originalRequest);
+      }
+      throw error;
+    }
+  );
+}
