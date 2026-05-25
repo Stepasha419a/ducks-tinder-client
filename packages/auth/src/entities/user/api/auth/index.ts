f94dbcfe82fcaca9317d0ad01a1ebade7db57f88
stepan <dark_magic419a@mail.ru>
2026-05-25 14:01:38 +0300

    feature: auth-package user-api keys

---
+++ packages/auth/src/entities/user/api/auth/index.ts
@@ -0,0 +1,5 @@
+export * from './keys';
+export * from './useLoginMutation';
+export * from './useRegisterMutation';
+export * from './useRefreshMutation';
+export * from './useLogoutMutation';
---
+++ packages/auth/src/entities/user/api/user/keys.ts
@@ -0,0 +1,3 @@
+export enum UserQueryKey {
+  User = 'user',
+}
