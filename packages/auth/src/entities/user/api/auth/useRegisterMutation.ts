0cb48e2ebce5cf4b909e343ba007f08dfab69b5c
stepan <dark_magic419a@mail.ru>
2026-05-25 14:01:16 +0300

    feature: auth-package auth-api use-register-mutation

---
+++ packages/auth/src/entities/user/api/auth/useRegisterMutation.ts
@@ -0,0 +1,33 @@
+import { useAuthStore } from '@entities/user';
+import { useMutation } from '@tanstack/react-query';
+import { AuthQueryKey } from './keys';
+import type { RegistrationParams } from '@shared/api';
+import { getAuthService } from '@shared/api';
+import { returnErrorMessage } from '@ducks-tinder-client/common';
+import { toast } from 'react-toastify';
+
+export function useRegisterMutation() {
+  const setAuthData = useAuthStore((state) => state.setAuthData);
+  const setIsAuth = useAuthStore((state) => state.setIsAuth);
+
+  return useMutation({
+    mutationKey: [AuthQueryKey.AuthMutation],
+    mutationFn: async ({ email, password, name }: RegistrationParams) => {
+      return getAuthService().registration(email, name, password);
+    },
+    onError: (error: unknown) => {
+      setAuthData(null);
+      setIsAuth(false);
+
+      // TODO: show 401 error with i18n keys mapping, fix prod 401 oauth response (overrides error message from auth-service)
+      toast(returnErrorMessage(error), { autoClose: 15000 });
+    },
+    onSuccess: (response) => {
+      const { accessToken, ...data } = response.data;
+      localStorage.setItem('accessToken', accessToken);
+
+      setAuthData(data);
+      setIsAuth(true);
+    },
+  });
+}
