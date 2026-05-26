69b9a2a7ddf963bc3e1ec6b945b785f2231fbbf0
stepan <dark_magic419a@mail.ru>
2026-05-26 15:58:31 +0300

    feature: auth-package storybook-main

---
+++ packages/auth/.storybook/main.ts
@@ -0,0 +1,34 @@
+import { withoutVitePlugins } from '@storybook/builder-vite';
+import type { StorybookConfig } from '@storybook/react-vite';
+import * as path from 'path';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.@(ts|tsx)'],
+  addons: ['@storybook/addon-links'],
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  framework: '@storybook/react-vite',
+  viteFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        alias: {
+          '@app': path.resolve('./src/app'),
+          '@pages': path.resolve('./src/pages'),
+          '@pages/*': path.resolve('./src/pages'),
+          '@widgets': path.resolve('./src/widgets/index'),
+          '@widgets/*': path.resolve('./src/widgets'),
+          '@hooks': path.resolve('./src/shared/lib/hooks'),
+          '@features': path.resolve('./src/features'),
+          '@entities': path.resolve('./src/entities'),
+          '@shared/constants': path.resolve('./src/shared/lib/constants'),
+          '@shared/helpers': path.resolve('./src/shared/lib/helpers'),
+          '@shared': path.resolve('./src/shared'),
+        },
+      },
+      plugins: await withoutVitePlugins(config.plugins, ['node-externals']),
+    };
+  },
+};
+export default config;
