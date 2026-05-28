e0dd6280e56285e0cc4de30a10adb96e30ab7bc2
stepan <dark_magic419a@mail.ru>
2026-05-28 21:02:47 +0300

    feature: auth-package call hocs

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,5 +1,33 @@
+import {
+  APP_AUTH_HOC_COMPOSITION,
+  APP_PRIVATE_HOC_COMPOSITION,
+  HocCompositionStage,
+} from '@ducks-tinder-client/common';
+import {
+  WithAuthRedirect,
+  WithInitialLoading,
+  WithUserData,
+} from '@entities/user/lib';
+
 export * from './entities/user';
 export * from './shared/api';
 
 export * from './features/LoginForm';
 export * from './features/RegistrationForm';
+
+APP_PRIVATE_HOC_COMPOSITION.addHocs(HocCompositionStage.BOOTSTRAPPING, [
+  WithInitialLoading,
+]);
+APP_PRIVATE_HOC_COMPOSITION.addHocs(HocCompositionStage.AUTH_CHECK, [
+  WithAuthRedirect,
+]);
+APP_PRIVATE_HOC_COMPOSITION.addHocs(HocCompositionStage.USER_HYDRATION, [
+  WithUserData,
+]);
+
+APP_AUTH_HOC_COMPOSITION.addHocs(HocCompositionStage.BOOTSTRAPPING, [
+  WithInitialLoading,
+]);
+APP_AUTH_HOC_COMPOSITION.addHocs(HocCompositionStage.AUTH_CHECK, [
+  WithAuthRedirect,
+]);
