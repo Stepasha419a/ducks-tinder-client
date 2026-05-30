e0da2b7dd2694f24694a2b2896b8bd035125e171
stepan <dark_magic419a@mail.ru>
2026-05-30 22:13:46 +0300

    feature: root-app remove initial-loading

--- apps/root/src/entities/user/ui/index.ts
+++ apps/root/src/entities/user/ui/index.ts
@@ -2,8 +2,6 @@ export { PairLink } from './PairLink/PairLink';
 export { LikesCount } from './LikesCount/LikesCount';
 export { Places } from './Places/Places';
 export { Map } from './Map/Map';
-export { InitialLoading } from './InitialLoading/InitialLoading';
-export { AuthLayout } from './AuthLayout/AuthLayout';
 export { SettingsGroup } from './SettingsGroup/SettingsGroup';
 export { NotFoundSetting } from './NotFoundSetting/NotFoundSetting';
 export { Explore } from './Explore/Explore';
