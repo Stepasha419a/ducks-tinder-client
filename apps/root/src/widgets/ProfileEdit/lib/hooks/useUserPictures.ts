b68f2eac3ad13fce25a67b69cbee9eddb6f3ad0c
stepan <dark_magic419a@mail.ru>
2026-06-02 11:45:50 +0300

    feature: root-app use-user-pictures use auth-stores

--- apps/root/src/widgets/ProfileEdit/lib/hooks/useUserPictures.ts
+++ apps/root/src/widgets/ProfileEdit/lib/hooks/useUserPictures.ts
@@ -1,17 +1,16 @@
 import { useEffect, useState } from 'react';
 
 import type { Picture } from '@ducks-tinder-client/common';
-import { useAppSelector } from '@ducks-tinder-client/common';
+import { useUserStore } from '@ducks-tinder-client/auth';
 
 export function useUserPictures() {
-  const userPictures = useAppSelector(
-    (state) => state.user.currentUser!.pictures
-  );
+  const userPictures = useUserStore((state) => state.currentUser?.pictures);
 
   const [pictures, setPictures] = useState<Picture[]>([]);
 
   useEffect(() => {
-    setPictures(userPictures);
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setPictures(userPictures || []);
   }, [userPictures]);
 
   return {
