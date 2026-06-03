6534b08dd382cb46ea128d0a68202d16b22d152d
stepan <dark_magic419a@mail.ru>
2026-06-03 17:12:11 +0300

    feature: root-app remove auth-hocs

--- apps/root/src/app/lib/hocs/index.ts
+++ apps/root/src/app/lib/hocs/index.ts
@@ -2,13 +2,13 @@ import {
   APP_AUTH_HOC_COMPOSITION,
   APP_PRIVATE_HOC_COMPOSITION,
   HocCompositionStage,
+  instance,
   WithErrorFallback,
+  WithTanstackQueryProvider,
 } from '@ducks-tinder-client/common';
 
-import { WithAuthRedirect } from '@features/WithAuthRedirect';
 import { WithCheckedFields } from '@features/WithCheckedFields';
-import { WithUserData } from '@features/WithUserData';
-import { WithInitialLoading, WithPairsInfo } from '@entities/user';
+import { WithPairsInfo } from '@entities/user';
 import { setUiLibSettings } from '@ducks-tinder-client/ui';
 
 export { WithBrowserRouter } from './BrowserRouter';
@@ -20,13 +20,7 @@ setUiLibSettings({ IMAGE_BASE_URL: window._env_.VAR_FILE_SERVICE_URL });
 setUpInterceptors(instance);
 
 APP_PRIVATE_HOC_COMPOSITION.addHocs(HocCompositionStage.BOOTSTRAPPING, [
-  WithInitialLoading,
-]);
-APP_PRIVATE_HOC_COMPOSITION.addHocs(HocCompositionStage.AUTH_CHECK, [
-  WithAuthRedirect,
-]);
-APP_PRIVATE_HOC_COMPOSITION.addHocs(HocCompositionStage.USER_HYDRATION, [
-  WithUserData,
+  WithTanstackQueryProvider,
 ]);
 APP_PRIVATE_HOC_COMPOSITION.addHocs(HocCompositionStage.DATA_SYNCING, [
   WithPairsInfo,
@@ -36,12 +30,6 @@ APP_PRIVATE_HOC_COMPOSITION.addHocs(HocCompositionStage.COMPLETE, [
   (Component) => WithErrorFallback(Component, { redirect: true }),
 ]);
 
-APP_AUTH_HOC_COMPOSITION.addHocs(HocCompositionStage.BOOTSTRAPPING, [
-  WithInitialLoading,
-]);
-APP_AUTH_HOC_COMPOSITION.addHocs(HocCompositionStage.AUTH_CHECK, [
-  WithAuthRedirect,
-]);
 APP_AUTH_HOC_COMPOSITION.addHocs(HocCompositionStage.COMPLETE, [
   (Component) => WithErrorFallback(Component, { redirect: true }),
 ]);
