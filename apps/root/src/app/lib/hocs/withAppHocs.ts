25e69d33416e564d8f37f5909627c82463eff575
stepan <dark_magic419a@mail.ru>
2026-06-03 17:12:31 +0300

    feature: root-app add tanstack-query-provider

--- apps/root/src/app/lib/hocs/withAppHocs.ts
+++ apps/root/src/app/lib/hocs/withAppHocs.ts
@@ -1,6 +1,14 @@
-import { compose } from '@ducks-tinder-client/common';
+import {
+  compose,
+  WithTanstackQueryProvider,
+} from '@ducks-tinder-client/common';
 
 import { StoreProvider } from '../providers';
 import { WithBrowserRouter } from './BrowserRouter';
 
-export const withAppHocs = compose(WithBrowserRouter, StoreProvider);
+export const withAppHocs = compose(
+  WithBrowserRouter,
+  StoreProvider,
+  // TODO: why do I have to call it here if I already call it inside hoc composition?
+  WithTanstackQueryProvider
+);
