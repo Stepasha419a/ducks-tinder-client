ddd4398dc15c7373904ebd55841dce15855c7471
stepan <dark_magic419a@mail.ru>
2026-06-04 17:21:31 +0300

    feature: chat-app use-messages-scroll use chat-store

--- apps/chat/src/entities/chat/lib/hooks/useMessagesScroll.ts
+++ apps/chat/src/entities/chat/lib/hooks/useMessagesScroll.ts
@@ -1,11 +1,11 @@
 import type { RefObject } from 'react';
 import { useCallback, useEffect, useRef } from 'react';
 
-import { useAppSelector } from '@ducks-tinder-client/common';
 import type { ControlRef } from '@ducks-tinder-client/ui';
+import { useChatSelector } from '@shared/lib/hooks';
 
 export function useMessagesScroll(controlRef: RefObject<ControlRef | null>) {
-  const messagesLength = useAppSelector((state) => state.chat.messages.length);
+  const messagesLength = useChatSelector((state) => state.chat.messages.length);
 
   const previousMessagesLength = useRef(0);
 
