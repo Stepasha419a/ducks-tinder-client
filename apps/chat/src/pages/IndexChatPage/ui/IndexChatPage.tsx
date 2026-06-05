77bd1c9a07cde5c9afb1a376596cd71c40d44b14
stepan <dark_magic419a@mail.ru>
2026-06-05 15:13:05 +0300

    feature: chat-app index-chat-page use chat-store

--- apps/chat/src/pages/IndexChatPage/ui/IndexChatPage.tsx
+++ apps/chat/src/pages/IndexChatPage/ui/IndexChatPage.tsx
@@ -1,23 +1,27 @@
 import type { FC } from 'react';
 
-import {
-  useAdaptiveMediaQuery,
-  useAppSelector,
-} from '@ducks-tinder-client/common';
+import { useAdaptiveMediaQuery } from '@ducks-tinder-client/common';
 
 import { ChatList } from '@entities/chat';
 
 import { Choose, Loading, NoChats } from './components';
+import { useChatSelector } from '@shared/lib/hooks';
+import { useUserStore } from '@ducks-tinder-client/auth';
 
 export const IndexChatPage: FC = () => {
+  const userId = useUserStore((state) => state.currentUser?.id);
+
   const isMobile = useAdaptiveMediaQuery('(max-width: 900px)');
 
-  const currentUserId = useAppSelector((state) => state.user.currentUser!.id);
-  const chatsLength = useAppSelector((state) => state.chat.chats.length);
-  const isFetched = useAppSelector((state) => state.chat.isFetched);
+  const chatsLength = useChatSelector((state) => state.chat.chats.length);
+  const isFetched = useChatSelector((state) => state.chat.isFetched);
+
+  if (!userId) {
+    return null;
+  }
 
   if (isMobile) {
-    return <ChatList currentUserId={currentUserId} />;
+    return <ChatList currentUserId={userId} />;
   }
 
   if (!chatsLength && isFetched) {
