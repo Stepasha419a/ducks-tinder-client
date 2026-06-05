a575458832f53642c81056d7c5433ed3d62e4016
stepan <dark_magic419a@mail.ru>
2026-06-05 15:12:45 +0300

    feature: chat-app chat-profile-popup use chat-store

--- apps/chat/src/widgets/ChatProfilePopup/ui/ChatProfilePopup.tsx
+++ apps/chat/src/widgets/ChatProfilePopup/ui/ChatProfilePopup.tsx
@@ -1,12 +1,12 @@
 import type { FC } from 'react';
 
-import { useAppSelector } from '@ducks-tinder-client/common';
 import { Popup, Preview } from '@ducks-tinder-client/ui';
 
 import { ChatControl } from '@features/ChatControl';
 
 import { ChatProfilePopupLazy } from './ChatProfilePopup.lazy';
 import * as styles from './ChatProfilePopup.module.scss';
+import { useChatSelector } from '@shared/lib/hooks';
 
 interface ChatProfilePopupProps {
   handleClose: () => void;
@@ -15,7 +15,7 @@ interface ChatProfilePopupProps {
 export const ChatProfilePopup: FC<ChatProfilePopupProps> = ({
   handleClose,
 }) => {
-  const chatMember = useAppSelector((state) => state.chat.chatMember);
+  const chatMember = useChatSelector((state) => state.chat.chatMember);
 
   return (
     <Popup closeHandler={handleClose} size="l" extraClassName={styles.popup}>
