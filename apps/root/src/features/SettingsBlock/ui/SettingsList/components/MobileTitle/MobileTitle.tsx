1c92c47bcd475f469b7ffaad98bab24199776cef
stepan <dark_magic419a@mail.ru>
2026-06-08 12:15:41 +0300

    feature: root-app mobile-title i18n

--- apps/root/src/features/SettingsBlock/ui/SettingsList/components/MobileTitle/MobileTitle.tsx
+++ apps/root/src/features/SettingsBlock/ui/SettingsList/components/MobileTitle/MobileTitle.tsx
@@ -3,13 +3,16 @@ import { Link } from 'react-router-dom';
 import { ROUTES } from '@ducks-tinder-client/common';
 
 import * as styles from './MobileTitle.module.scss';
+import { useTranslation } from 'react-i18next';
 
 export const MobileTitle = () => {
+  const { t } = useTranslation();
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.title}>Settings</div>
+      <div className={styles.title}>{t('profile.settingsMobile')}</div>
       <Link to={ROUTES.PROFILE} className={styles.submit}>
-        Submit
+        {t('submit')}
       </Link>
     </div>
   );
