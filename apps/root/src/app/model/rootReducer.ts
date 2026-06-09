1adf2d687ef73cc9eea4443fa2d87380c329a35f
stepan <dark_magic419a@mail.ru>
2026-06-09 18:41:46 +0300

    feature: root-app use chat-reducer from chat-app

--- apps/root/src/app/model/rootReducer.ts
+++ apps/root/src/app/model/rootReducer.ts
@@ -1,17 +1,14 @@
+import type { Reducer } from '@reduxjs/toolkit';
 import { combineReducers } from '@reduxjs/toolkit';
 
-import {
-  authReducer,
-  chatReducer,
-  userReducer,
-} from '@ducks-tinder-client/common';
+import { userReducer } from '@ducks-tinder-client/common';
 
 import { pairReducer, tinderReducer } from '@entities/user';
+import { chatReducer } from 'chatApp/models';
 
 export const rootReducer = combineReducers({
   user: userReducer,
   pair: pairReducer,
-  auth: authReducer,
-  chat: chatReducer,
+  chat: chatReducer as Reducer<unknown>,
   tinder: tinderReducer,
 });
