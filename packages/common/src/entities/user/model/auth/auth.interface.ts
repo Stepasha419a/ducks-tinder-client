a2ca80548225c2e0c27593cce3d472e10349dcca
stepan <dark_magic419a@mail.ru>
2026-06-11 17:35:41 +0300

    refactor: common-package remove auth slice

--- packages/common/src/entities/user/model/auth/auth.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface AuthData {
-  id: string;
-  email: string;
-  createdAt: string;
-  updatedAt: string;
-}
--- packages/common/src/entities/user/model/auth/index.ts
+++
@@ -1,7 +0,0 @@
-export {
-  checkAuthThunk,
-  loginThunk,
-  logoutThunk,
-  registerThunk,
-} from './auth.thunks';
-export { authReducer } from './auth.slice';
