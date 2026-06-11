861fdf6908623fdbaef96d511e219b6c99367dda
stepan <dark_magic419a@mail.ru>
2026-06-11 17:34:35 +0300

    refactor: common-package remove chat, auth reducers

--- packages/common/src/app/model/rootReducer.ts
+++ packages/common/src/app/model/rootReducer.ts
@@ -1,10 +1,7 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
-import { chatReducer } from '@entities/chat';
-import { authReducer, userReducer } from '@entities/user';
+import { userReducer } from '@entities/user';
 
 export const rootReducer = combineReducers({
-  chat: chatReducer,
-  auth: authReducer,
   user: userReducer,
 });
