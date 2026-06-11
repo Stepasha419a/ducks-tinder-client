0794ab7b97bda599da4c8497bf437bfe8741b2a0
stepan <dark_magic419a@mail.ru>
2026-06-11 18:15:19 +0300

    refactor: common-package remove axios interceptors

--- packages/common/src/shared/api/instance.ts
+++ packages/common/src/shared/api/instance.ts
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
-import { setUpInterceptors } from './interceptors';
-
 export const instance = axios.create({
   withCredentials: true,
   paramsSerializer: { indexes: null },
 });
-
-setUpInterceptors(instance);
