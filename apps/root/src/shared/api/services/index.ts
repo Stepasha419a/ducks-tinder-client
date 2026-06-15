fe1e440f674625003b4da538e8093daf6879791f
stepan <dark_magic419a@mail.ru>
2026-06-15 15:15:50 +0300

    feature: root-app service-getter

---
+++ apps/root/src/shared/api/services/index.ts
@@ -0,0 +1,28 @@
+export * from './mock';
+
+export * from './user/user-service.interface';
+
+import type { UserService } from './user/user-service.interface';
+import { createUserService } from './user/user.service';
+
+interface Instances {
+  userService: UserService | null;
+}
+
+const instances: Instances = {
+  userService: null,
+};
+
+export const getOrCreateService = <T extends Instances[keyof Instances]>(
+  key: keyof Instances,
+  createService: () => T
+): T => {
+  if (!instances[key]) {
+    instances[key] = createService() as UserService;
+  }
+
+  return instances[key] as T;
+};
+
+export const getUserService = () =>
+  getOrCreateService('userService', createUserService);
