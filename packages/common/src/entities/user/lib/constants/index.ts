0067a1b259e764d66ce3c91bca0e904311dd3316
stepan <dark_magic419a@mail.ru>
2026-06-17 12:19:46 +0300

    refactor: common-package remove user-lib

--- packages/common/src/entities/user/lib/constants/index.ts
+++
@@ -1,1 +0,0 @@
-export { SettingNameEnum } from './settingName';
--- packages/common/src/entities/user/lib/constants/settingName.ts
+++
@@ -1,9 +0,0 @@
-export enum SettingNameEnum {
-  EMAIL = 'email',
-  NAME = 'name',
-  DESCRIPTION = 'description',
-  SEX = 'sex',
-  PREFER_SEX = 'preferSex',
-  NICKNAME = 'nickname',
-  LANGUAGE = 'language',
-}
