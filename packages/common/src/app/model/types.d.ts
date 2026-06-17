f4c7ed91d7a78b627b1bc30cde40b8a4fca51bd0
stepan <dark_magic419a@mail.ru>
2026-06-17 12:19:54 +0300

    refactor: common-package remove store types

--- packages/common/src/app/model/types.d.ts
+++
@@ -1,7 +0,0 @@
-import type { store } from './store';
-
-declare global {
-  export type RootStore = ReturnType<typeof store>;
-  export type RootState = ReturnType<typeof store.getState>;
-  export type AppDispatch = typeof store.dispatch;
-}
