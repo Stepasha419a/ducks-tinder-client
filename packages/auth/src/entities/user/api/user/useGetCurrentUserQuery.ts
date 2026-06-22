ee39e198f746a05d226fa6ddabe96cf679bda47b
stepan <dark_magic419a@mail.ru>
2026-06-22 11:14:41 +0300

    feature: auth-package use-get-current-user-query call after success auth

--- packages/auth/src/entities/user/api/user/useGetCurrentUserQuery.ts
+++ packages/auth/src/entities/user/api/user/useGetCurrentUserQuery.ts
@@ -1,14 +1,26 @@
 import { useQuery } from '@tanstack/react-query';
 import { UserQueryKey } from './keys';
 import { getUserGetMeEndpoint } from '@shared/api';
+import { isAxiosError } from 'axios';
+import { useAuthStore } from '@entities/user/model';
 
 export function useGetCurrentUserQuery() {
+  const isAuth = useAuthStore((state) => state.isAuth);
+
   return useQuery({
     queryKey: [UserQueryKey.User],
     queryFn: async () => {
       return getUserGetMeEndpoint()();
     },
     select: (response) => response.data,
+    retry: (failureCount, error: unknown) => {
+      if (isAxiosError(error) && error.response?.status === 401) {
+        return false;
+      }
+
+      return failureCount < 3;
+    },
     retryDelay: 2000,
+    enabled: Boolean(isAuth),
   });
 }
