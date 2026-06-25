4b2d1f1bd4a937e0ef342dc20618d295c5a3bb6c
stepan <dark_magic419a@mail.ru>
2026-06-25 11:23:14 +0300

    feature: scripts validate-i18n get-deep-keys

--- scripts/validate-i18n.js
+++ scripts/validate-i18n.js
@@ -6,3 +6,15 @@ const BASE_LANG = 'en';
 const MFEs_PATH = 'apps';
 
 let hasErrors = false;
+
+function getDeepKeys(obj, prefix = '') {
+  return Object.keys(obj).reduce((res, el) => {
+    if (Array.isArray(obj[el])) return res;
+    if (typeof obj[el] === 'object' && obj[el] !== null) {
+      res.push(...getDeepKeys(obj[el], prefix + el + '.'));
+    } else {
+      res.push(prefix + el);
+    }
+    return res;
+  }, []);
+}
