b485ad2be28d1b5ac95e942ed7a24960f2633084
stepan <dark_magic419a@mail.ru>
2026-06-26 16:44:13 +0300

    feature: root-app german setting

--- apps/root/src/shared/lib/constants/languages.ts
+++ apps/root/src/shared/lib/constants/languages.ts
@@ -1,9 +1,11 @@
 export enum SupportedLanguage {
   English = 'en',
   Russian = 'ru',
+  German = 'de',
 }
 
 export const LANGUAGE_TRANSCRIPTION: Record<SupportedLanguage, string> = {
   [SupportedLanguage.English]: 'English',
   [SupportedLanguage.Russian]: 'Русский',
+  [SupportedLanguage.German]: 'Deutsch',
 };
