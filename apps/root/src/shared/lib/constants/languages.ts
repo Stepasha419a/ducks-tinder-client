f727387dbfe7b0e04e50a5745b26d08d518bf900
stepan <dark_magic419a@mail.ru>
2026-07-01 08:55:23 +0300

    feature: root-app add chinese language

--- apps/root/src/shared/lib/constants/languages.ts
+++ apps/root/src/shared/lib/constants/languages.ts
@@ -2,10 +2,12 @@ export enum SupportedLanguage {
   English = 'en',
   Russian = 'ru',
   German = 'de',
+  Chinese = 'zh',
 }
 
 export const LANGUAGE_TRANSCRIPTION: Record<SupportedLanguage, string> = {
   [SupportedLanguage.English]: 'English',
   [SupportedLanguage.Russian]: 'Русский',
   [SupportedLanguage.German]: 'Deutsch',
+  [SupportedLanguage.Chinese]: '中文',
 };
