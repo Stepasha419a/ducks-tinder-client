6d04ed992a1d8b1efd64d6bbe9d390e8ffc2ff5b
stepan <dark_magic419a@mail.ru>
2026-07-02 09:31:51 +0300

    refactor: root-app remove node vars

--- apps/root/src/app/lib/types/react.d.ts
+++ apps/root/src/app/lib/types/react.d.ts
@@ -1,13 +1,6 @@
 /// <reference types="node" />
 /// <reference types="react-dom" />
 
-declare namespace NodeJS {
-  interface ProcessEnv {
-    readonly NODE_ENV: 'development' | 'production' | 'test';
-    readonly PUBLIC_URL: string;
-  }
-}
-
 declare module '*.avif' {
   const src: string;
   export default src;
