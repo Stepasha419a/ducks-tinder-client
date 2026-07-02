57d253cd8df44b8b60560e9f4cb05fffcd2aab6d
stepan <dark_magic419a@mail.ru>
2026-07-02 09:32:16 +0300

    refactor: common-package remove node vars

--- packages/common/src/app/lib/types/react.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node" />
-
-declare namespace NodeJS {
-  interface ProcessEnv {
-    readonly NODE_ENV: 'development' | 'production' | 'test';
-    readonly PUBLIC_URL: string;
-  }
-}
