daa7eaab92ddc0edb918f8b4ab40561ef6abcc29
stepan <dark_magic419a@mail.ru>
2026-07-13 09:32:17 +0300

    feature: ui-package add hocs to composition

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,10 @@
+import {
+  APP_AUTH_HOC_COMPOSITION,
+  APP_PRIVATE_HOC_COMPOSITION,
+  HocCompositionStage,
+} from '@ducks-tinder-client/common';
+import { WithModals } from '@shared/lib/hocs';
+
 import './app/styles/index.scss';
 
 export * from './shared/ui';
@@ -5,3 +12,11 @@ export * from './shared/model';
 export * from './shared/lib';
 
 export * from './entities/user';
+
+APP_PRIVATE_HOC_COMPOSITION.addHocs(HocCompositionStage.BOOTSTRAPPING, [
+  WithModals,
+]);
+
+APP_AUTH_HOC_COMPOSITION.addHocs(HocCompositionStage.BOOTSTRAPPING, [
+  WithModals,
+]);
