de9986b86fb2d25123d2cd21dd1cbb355402e5f4
stepan <dark_magic419a@mail.ru>
2026-07-16 16:43:08 +0300

    refactor: root-app filter-pair-items ui-modal

--- apps/root/src/features/FilterPairsItems/FilterPairsItems.tsx
+++ apps/root/src/features/FilterPairsItems/FilterPairsItems.tsx
@@ -1,10 +1,10 @@
-import type { Dispatch, FC, SetStateAction } from 'react';
+import type { FC } from 'react';
 import type { Control } from 'react-hook-form';
 import { faSliders } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { useAdaptiveMediaQuery } from '@ducks-tinder-client/common';
-import { ListItemButton } from '@ducks-tinder-client/ui';
+import { ListItemButton, useOpenModal } from '@ducks-tinder-client/ui';
 
 import type { PairFilterForm } from '@entities/user';
 import { INTERESTS_FOR_LOOP } from '@entities/user';
@@ -12,21 +12,45 @@ import { INTERESTS_FOR_LOOP } from '@entities/user';
 import { useOuterPairFilterForm } from './lib';
 import * as styles from './FilterPairsItems.module.scss';
 import { useTranslation } from 'react-i18next';
+import {
+  PairsFilterPopup,
+  type PairsFilterPopupProps,
+  type PairsFilterPopupReturn,
+} from '@features/PairsFilterPopup';
 
 interface FilterPairsItemsProps {
   isFilterPopupOpen: boolean;
-  setIsFilterPopupOpen: Dispatch<SetStateAction<boolean>>;
   control: Control<PairFilterForm>;
   handleSubmit: () => void;
+  handleReset: () => void;
 }
 
 export const FilterPairsItems: FC<FilterPairsItemsProps> = ({
   isFilterPopupOpen,
-  setIsFilterPopupOpen,
   control,
   handleSubmit,
+  handleReset,
 }) => {
   const { t } = useTranslation();
+
+  const { openModal } = useOpenModal();
+
+  const handleOpenFilterPopup = async () => {
+    const result = await openModal<
+      PairsFilterPopupProps,
+      PairsFilterPopupReturn
+    >({
+      Component: PairsFilterPopup,
+      props: { control },
+    });
+
+    if (result?.isSubmit) {
+      handleSubmit();
+    }
+    if (result?.isReset) {
+      handleReset();
+    }
+  };
 
   const isSmallMobile = useAdaptiveMediaQuery('(max-width: 600px)');
 
@@ -41,7 +65,7 @@ export const FilterPairsItems: FC<FilterPairsItemsProps> = ({
     <>
       <div className={styles.filtering}>
         <ListItemButton
-          onClick={() => setIsFilterPopupOpen(true)}
+          onClick={handleOpenFilterPopup}
           isActive={isFilterPopupOpen}
           extraClassName={styles.item}
         >
