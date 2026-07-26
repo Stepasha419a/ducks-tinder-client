4bd156b97265a7044b5937bbc76161c2a1d9d255
stepan <dark_magic419a@mail.ru>
2026-07-26 20:25:18 +0300

    feature: root-app subscription-service-url env

--- apps/root/src/app/lib/types/env.d.ts
+++ apps/root/src/app/lib/types/env.d.ts
@@ -9,6 +9,7 @@ declare global {
       VAR_CHAT_SERVICE_SOCKET_PATH: string;
       VAR_FILE_SERVICE_URL: string;
       VAR_AUTH_SERVICE_URL: string;
+      VAR_SUBSCRIPTION_SERVICE_URL: string;
       VAR_MODE: string;
       VAR_ROOT_PATH: string;
       VAR_MATOMO_URL?: string;
