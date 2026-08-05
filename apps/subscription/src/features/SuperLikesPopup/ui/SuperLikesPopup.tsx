949cc68d102a3d9b6de2d21fe6f3dd07c52dd164
stepan <dark_magic419a@mail.ru>
2026-08-05 08:44:43 +0300

    feature: subscription-app super-likes-popup star-icon

--- apps/subscription/src/features/SuperLikesPopup/ui/SuperLikesPopup.tsx
+++ apps/subscription/src/features/SuperLikesPopup/ui/SuperLikesPopup.tsx
@@ -8,6 +8,8 @@ import {
 } from '@ducks-tinder-client/ui';
 
 import * as styles from './SuperLikesPopup.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 interface PricingPlan {
   id: number;
@@ -51,31 +53,7 @@ export const SuperLikesPopup = () => {
     >
       <div className={styles.content}>
         <div className={styles.iconWrapper}>
-          <div className={styles.starIcon}>
-            <svg
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z"
-                fill="url(#starGradient)"
-              />
-              <defs>
-                <linearGradient
-                  id="starGradient"
-                  x1="2"
-                  y1="2"
-                  x2="22"
-                  y2="22"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop stopColor="#50b3ff" />
-                  <stop offset="1" stopColor="#3d5afe" />
-                </linearGradient>
-              </defs>
-            </svg>
-          </div>
+          <FontAwesomeIcon icon={faStar} className={styles.starIcon} />
         </div>
 
         <h2 className={styles.title}>Get Super Likes</h2>
